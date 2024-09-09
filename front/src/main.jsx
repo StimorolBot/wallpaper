b24e94cd0bf1bcc2b37d72a8756e7986cc5dbc41
bot <maximBotPy@gmail.com>
2024-09-09 13:52:56 +0300

    init

---
+++ front/src/main.jsx
@@ -0,0 +1,8 @@
+import router from "../router"
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from "react-router-dom"
+
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={ router } />
+)
