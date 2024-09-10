d1d6dd06c5d7eb2d664fc61fa66a9118c1ac20e3
bot <maximBotPy@gmail.com>
2024-09-10 16:46:37 +0300

    добавил класс для создания пользователя

---
+++ back/src/app/auth/user_manager.py
@@ -0,0 +1,48 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends, HTTPException, status
+
+from core.crud import crud
+from core.mode import ModeRead
+from src.db.get_session import get_async_session
+from core.abs_model.model import UserManagerABC
+
+from src.app.auth.schemas import Login
+from src.app.auth.model import AuthTable
+from src.app.auth.password_auth import password_auth
+
+
+class UserManager(UserManagerABC):
+
+    async def auth(self, auth_login: Login, session: AsyncSession = Depends(get_async_session)):
+        unauthorized_error_msg = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный логин/пароль"
+        )
+
+        user = await crud.read(
+            session=session, table=AuthTable,
+            field=AuthTable.email, value=auth_login.email, mode=ModeRead.ONE.value
+        )
+        if not user:
+            raise unauthorized_error_msg
+
+        is_verify_password = password_auth.verify_password(
+            hash_password=user.hash_password, password=auth_login.password
+        )
+        if is_verify_password is False:
+            raise unauthorized_error_msg
+
+        return user
+
+    @staticmethod
+    def user_config(auth_dict_user: dict) -> dict:
+        auth_dict_user["hash_password"] = password_auth.get_hash_password(auth_dict_user["password"])
+        auth_dict_user["is_active"] = False
+        auth_dict_user["is_superuser"] = False
+        auth_dict_user["is_verified"] = False
+        del auth_dict_user["password"]
+
+        return auth_dict_user
+
+
+user_manager = UserManager()
