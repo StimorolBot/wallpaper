2e1d32624e302a4e489cbba58316fe8313e51733
bot <maximBotPy@gmail.com>
2024-09-18 21:41:27 +0300

    добавил пути для popular и register

--- front/src/router.jsx
+++ front/src/router.jsx
@@ -1,19 +1,21 @@
-import { Reg } from "./page/Reg"
 import { Home } from "./page/Home"
 import { Error } from "./page/Error"
+import { Popular } from "./page/Popular"
 import { Login } from "./page/auth/Login"
-import { CreateImg } from "./page/CreateImg"
-import { createBrowserRouter } from "react-router-dom"
 import { AboutImg } from "./page/AboutImg"
+import { CreateImg } from "./page/CreateImg"
+import { Register } from "./page/auth/Register"
+import { createBrowserRouter } from "react-router-dom"
 
 
 const router = createBrowserRouter([
     { path: "/", element: <Home/>},
     { path: "/create", element: <CreateImg/>},
     { path: "/wallpaper/:uuid", element: <AboutImg/>},
+    { path: "/popular", element: <Popular/>},
     { path: "/auth", children: [
       { path: "login", element: <Login/>},
-      { path: "register", element: <Reg/>},
+      { path: "register", element: <Register/>},
       { path: "", element: <Error errorCode={404} errorMsg={"Не далось найти страницу"}/>}
     ]},
     { path: "*", element: <Error/>},
