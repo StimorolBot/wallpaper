b1209a80689811f84d0f938d66053c0491f7dfa6
bot <maximBotPy@gmail.com>
2024-09-22 23:07:24 +0300

    feat(set_reaction): добавил запрос к бд для установки реакции

--- back/src/app/img/router.py
+++ back/src/app/img/router.py
@@ -1,7 +1,7 @@
 from fastapi_cache.decorator import cache
 from fastapi import APIRouter, status, Depends, HTTPException
 
-from sqlalchemy import select, func
+from sqlalchemy import select, func, and_, case
 from sqlalchemy.sql.functions import concat
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.dialects.postgresql import INTERVAL
@@ -14,7 +14,10 @@ from src.app.img.get_user import get_user_by_token
 from src.app.img.model import ImgTable
 from src.app.img.generate_img import api
 from src.app.img.schemas import ImageSchemas
+from src.app.img.enums.filter_time import FilterTime
+from src.app.img.enums.status_r import StatusReaction
 from src.app.img.reaction.model import ReactionTable
+from src.app.img.reaction.schemas import ReactionSchemas
 from src.app.img.reaction.count_reaction_query import count_reaction
 
 from src.app.auth.model import AuthTable
@@ -45,21 +48,28 @@ async def create_img(
 
 @img_router.get("/")
 @cache(expire=120)
-async def get_all_img(session: AsyncSession = Depends(get_async_session)):
+async def get_all_img(
+        user=Depends(get_user_by_token),
+        session: AsyncSession = Depends(get_async_session)
+):
     img_list = await crud.read(session=session, table=ImgTable)
     return img_list
 
 
 @img_router.get("/popular")
-@cache(expire=10)
-async def get_popular_img(session: AsyncSession = Depends(get_async_session)):
+@cache(expire=120)
+async def get_popular_img(filter_time: FilterTime, session: AsyncSession = Depends(get_async_session)):
     query = (
         select(
             ImgTable
         )
-        .where(ImgTable.create_date >= func.now() - func.cast(concat(10, ' DAYS'), INTERVAL))
+        .where(ImgTable.create_date >= func.now() - func.cast(concat(1, f" {filter_time.value}"), INTERVAL))
     )
     r = await session.execute(query)
+
+    if not r:
+        ...
+    # с uhg баг
     return get_item(r)
 
 
@@ -112,9 +122,71 @@ async def get_img(uuid_img: str, session: AsyncSession = Depends(get_async_sessi
 
 @img_router.post("/set-reaction")
 async def set_reaction(
-    uuid_img: str,
-    user: dict = Depends(get_user_by_token),
-    session: AsyncSession = Depends(get_async_session)
+        reaction: ReactionSchemas,
+        session: AsyncSession = Depends(get_async_session),
+        user: dict = Depends(get_user_by_token)
 ):
-    ...
+    query_case = (
+        case(
+            (ReactionTable.uuid_user == user["sub"],
+             case(
+                 (ReactionTable.uuid_img == reaction.img_uuid,
+                  case(
+                      (ReactionTable.reaction == reaction.reaction, StatusReaction.EQUAL_REACTIONS.value),
+                      else_=StatusReaction.DIFFERENT_REACTIONS.value
+                  )),
+                 else_=StatusReaction.NO_REACTION.value
+             )),
+            else_=StatusReaction.NO_REACTION.value,
+        )
+    )
 
+    query = (
+        select(query_case)
+        .where(and_(
+            ReactionTable.uuid_user == user["sub"],
+            ReactionTable.uuid_img == reaction.img_uuid)
+        )
+    )
+
+    result = await session.execute(query)
+    result = result.unique().all()
+
+    if not result:
+        await crud.create(
+            session=session,
+            table=ReactionTable,
+            data={"uuid_user": user["sub"], "uuid_img": reaction.img_uuid, "reaction": reaction.reaction}
+        )
+        return
+
+    match result[-1][0]:
+        case StatusReaction.EQUAL_REACTIONS.value:
+            await crud.delite(
+                session=session,
+                table=ReactionTable,
+                field=ReactionTable.uuid_user,
+                field_val=user["sub"],
+                field_2=ReactionTable.uuid_img,
+                field_val_2=reaction.img_uuid
+            )
+            print('запись удалена')
+        case StatusReaction.DIFFERENT_REACTIONS.value:
+            await crud.update(
+                session=session,
+                table=ReactionTable,
+                field=ReactionTable.uuid_user,
+                field_val=user["sub"],
+                field_2=ReactionTable.uuid_img,
+                field_val_2=reaction.img_uuid,
+                data={"reaction": reaction.reaction}
+            )
+            print('запись обновлена')
+
+        case StatusReaction.NO_REACTION.value:
+            await crud.create(
+                session=session,
+                table=ReactionTable,
+                data={"uuid_user": user["sub"], "uuid_img": reaction.img_uuid, "reaction": reaction.reaction}
+            )
+            print('запись создана')
