f80356fd9f4a51eab76b98dcb10f2b55609d1660
bot <maximBotPy@gmail.com>
2024-09-27 00:27:44 +0300

    fix: исправил отображение всех пунктов

--- front/src/page/AboutImg.jsx
+++ front/src/page/AboutImg.jsx
@@ -1,19 +1,24 @@
-import api from "../api"
+import { api } from "../api"
 import { useEffect, useState } from "react"
 import { Header } from "../components/header/Header"
-import { InputReaction } from "../components/ui/input/InputReaction"
 
 import "./style/about_img.sass"
 
 
 export function AboutImg(){
     const [imgInfo, setImgInfo] = useState("")
-    const uuidImg = window.location.pathname.split("wallpaper/")[1]     
-
+    const uuidImg = window.location.pathname.split("wallpaper/")[1]
+    
+    const setReaction = async (event) => {
+        event.preventDefault()
+    // вынести в отдельную ф. 
+    }
+  
+    
     useEffect(() => {(
         async () => {
-            await api.get(`/wallpaper/${uuidImg}`).then((response) => {            
-                setImgInfo(response.data)
+            await api.get(`/wallpaper/${uuidImg}`).then((response) => {
+                setImgInfo({...response.data})
             }).catch((error) =>{
                 console.log(error)
             })
@@ -23,45 +28,67 @@ export function AboutImg(){
     return(
         <>
         <Header/>
-        <div className="wrapper">
-            <div className="about-img__container">
-                <img className="about__img"
-                    src={"data:image/jpeg;base64," + imgInfo["img_base64"]} alt="img"
-                />
+        <section className="about-img section_flex">
+            <h2 className="hidden">
+                Информация о изображении
+            </h2>
+            <div className="wrapper">
+                <div className="about-img__container">
+                    <div className="about__img-container">
+                        <img className="about__img"
+                            src={"data:image/jpeg;base64," + imgInfo["img_base64"]} alt="img"
+                        />
+                    </div>
+                    <div className="about__list">
+                        <div className="about__list-container">
+                            <dt className="about__list-dt">Автор:</dt>
+                            <dd className="about__list-dd">{imgInfo["user_name"]}</dd>
+                        </div>
 
-                <dl className="about__list">
-                    <dt className="about__list-item"> Автор </dt>
-                    <dd>Ник</dd>
+                        <div className="about__list-container">
+                            <dt className="about__list-dt">Стиль:</dt>
+                            <dd className="about__list-dd">
+                                {imgInfo["style"]}
+                            </dd>
+                        </div>
 
-                    <dt>Дата создания</dt>
-                    <dd className="about__list-item">
-                        <time dateTime={imgInfo["create_date"]}>
-                            Время
-                        </time>
-                    </dd>
+                        <div className="about__list-container">
+                            <dt className="about__list-dt">Дата создания:</dt>
+                            <dd className="about__list-dd">
+                                <time dateTime={imgInfo["create_date"]}>
+                                    {imgInfo["create_date"]?.split("T")[0]}
+                                </time>
+                            </dd>
+                        </div>
 
-                    <dt>Промт</dt>
-                    <dd className="about__list-item">
-                        {imgInfo["prompt"]}
-                    </dd>
-                </dl>
-                
-                <div className="reaction__container">
-                    <InputReaction>
-                        <img className="reaction-img" src="/static/like.svg" alt="like" />
-                    </InputReaction>
-                    
-                    <InputReaction>
-                        <img className="reaction-img" src="/static/dislike.svg" alt="dislike" />
-                    </InputReaction>
+                        <div className="about__list-container">
+                            <dt className="about__list-dt">Промт:</dt>
+                            <dd className="about__list-dd about__list-promt">
+                                {imgInfo["prompt"]}
+                            </dd>
+                        </div>
+
+                        <div className="reaction__container">
+                            <div className="reaction__item">
+                                <img className="reaction-img" src="/static/like.svg" alt="like" />
+                                <p className="reaction-count">{imgInfo["like"]}</p>
+                            </div>
+
+                            <div className="reaction__item">
+                                <img className="reaction-img" src="/static/dislike.svg" alt="dislike" />
+                                <p className="reaction-count">{imgInfo["dislike"]}</p>
+                            </div>        
+                        </div>
+                        <div className="download-img__container">
+                            <a className="download-img-link hover" download={`${uuidImg}.png`}
+                                href={"data:image/jpeg;base64," + imgInfo["img_base64"]}>
+                                Скачать
+                            </a>
+                        </div> 
+                    </div>
                 </div>
             </div>
-            
-            <a className="download-img-link" download={`${uuidImg}.png`}
-                href={"data:image/jpeg;base64," + imgInfo["img_base64"]}>
-                    Скачать
-            </a>
-        </div>
+        </section>
         </>
     )
 }
