3a8407789ff364864f9626a8310a9f5dce6da19d
bot <maximBotPy@gmail.com>
2024-09-29 00:10:23 +0300

    fix: исправил опечатку feat: добавил relationship

--- back/src/app/img/model.py
+++ back/src/app/img/model.py
@@ -5,21 +5,22 @@ from sqlalchemy import func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.model import Base
-from core.generate_uuid import generate_uuid
+from core.help import generate_uuid
 
 if TYPE_CHECKING:
     from src.app.auth.model import AuthTable
+    from src.app.img.reaction.model import ReactionTable
 
 
 class ImgTable(Base):
     __tablename__ = "img_table"
 
-    uuid: Mapped[str] = mapped_column(primary_key=True, default=generate_uuid, index=True, unique=True)
-    uuid_img: Mapped[str] = mapped_column(unique=True, index=True, default=generate_uuid)
-    uuid_user: Mapped[str] = mapped_column(ForeignKey("user_table.uuid"))
+    uuid_img: Mapped[str] = mapped_column(primary_key=True, default=generate_uuid, index=True, unique=True)
+    uuid_user: Mapped[str] = mapped_column(ForeignKey("user_table.uuid_user"))
     style: Mapped[str] = mapped_column()
     create_date: Mapped[datetime] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
     prompt: Mapped[str] = mapped_column()
     img_base64: Mapped[str] = mapped_column()
 
     user_relationship: Mapped["AuthTable"] = relationship(back_populates="img_relationship")
+    reaction_relationship: Mapped["ReactionTable"] = relationship(back_populates="img_reaction_relationship")
