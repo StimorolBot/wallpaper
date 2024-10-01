f92cd53653b58656faddc3a147e894f045800307
bot <maximBotPy@gmail.com>
2024-10-01 21:56:53 +0300

    refactor: заменил относительный путь к env на path

--- back/src/app/img/config.py
+++ back/src/app/img/config.py
@@ -1,4 +1,7 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+PATH = f"{os.path.dirname(os.path.abspath(__file__))}/.env"
 
 
 class Settings(BaseSettings):
@@ -6,7 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     BASE_URL: str = "https://api-key.fusionbrain.ai/"
 
-    model_config = SettingsConfigDict(env_file="./src/app/img/.env")
+    model_config = SettingsConfigDict(env_file=PATH)
 
 
 settings = Settings()
