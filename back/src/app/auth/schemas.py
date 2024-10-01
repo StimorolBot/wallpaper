c3bd47228e57c4eaf918ea237ff0f30a99231acd
bot <maximBotPy@gmail.com>
2024-10-01 21:36:38 +0300

    feat: добавил валидацию для пароля, почты, сброса пароля

--- back/src/app/auth/schemas.py
+++ back/src/app/auth/schemas.py
@@ -1,26 +1,52 @@
 from typing_extensions import Annotated
 from fastapi import status, HTTPException
-from pydantic import BaseModel, ConfigDict, EmailStr, WrapValidator
+from core.help import valid_forbidden_symbols, valid_len
+from pydantic import BaseModel, ConfigDict, WrapValidator
 
 
 def valid_name(name: str, handler) -> str:
-    if len(name) < 2 or len(name) > 20:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Имя пользователя должен быть в пределах от 2 до 20 символов")
+    valid_len(name, 4, 20)
+    valid_forbidden_symbols(name)
+
     return name
 
 
+def valid_password(password: str, handler) -> str:
+    valid_len(password, 8, 32)
+    valid_forbidden_symbols(password)
+
+    return password
+
+
+def valid_email(email: str, handler) -> str:
+    error = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Некорректный формат почты")
+
+    if len(email.split("@")) != 2:
+        raise error
+    elif len(email.split(".")) != 2:
+        raise error
+
+    valid_len(email, 10, 40)
+    valid_forbidden_symbols(email, email=True)
+    return email
+
+
 ValidName = Annotated[str, WrapValidator(valid_name)]
+ValidEmail = Annotated[str, WrapValidator(valid_email)]
+ValidPassword = Annotated[str, WrapValidator(valid_password)]
 
 
-class Login(BaseModel):
-    email: EmailStr
-    password: str
+class Email(BaseModel):
+    email: ValidEmail
+
+
+class Login(Email):
+    password: ValidPassword
 
 
 class Register(Login):
     user_name: ValidName
+    code_confirm: str
     is_active: bool = False
     is_superuser: bool = False
     is_verified: bool = False
@@ -28,6 +54,11 @@ class Register(Login):
     model_config = ConfigDict(from_attributes=True)
 
 
-class RegisterDTO(BaseModel):
+class RegisterDTO(Email):
     user_name: ValidName
-    email: EmailStr
+
+
+class ResetPassword(Email):
+    password: ValidPassword
+    code_confirm: str
+
