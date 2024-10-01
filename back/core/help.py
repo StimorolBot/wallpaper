3cac897b2650d4be28c0677802be7178e37fa7c6
bot <maximBotPy@gmail.com>
2024-10-01 22:48:09 +0300

    feat: добавил функции проверки длины строки, наличие запрещенных символов, генерации кода, установки значений в редис и их получения

--- back/core/help.py
+++ back/core/help.py
@@ -1,4 +1,9 @@
+import json
+import string
+import secrets
 from uuid import uuid4
+from src.conf import redis
+from fastapi import status, HTTPException
 
 
 def generate_uuid() -> str:
@@ -11,3 +16,43 @@ def create_dict(list_key: list, list_value: list) -> dict:
 
 def get_item(main_list) -> list:
     return [item for items in main_list for item in items]
+
+
+def valid_forbidden_symbols(val: str, email: bool = False):
+    symbols = {
+        "[", "]", "\\", "^", "$", "|", "?", "*", "+", "(", ")",
+        "{", "}", "/", "#", "'", '"', "@", " ", "-", "!", "~",
+        "`", ".", ",", "%", "=", "№", "&"
+    }
+
+    if email:
+        symbols.remove("@")
+        symbols.remove(".")
+
+    if symbols & set(val):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Поле не должно содержать: {symbols}")
+
+
+def valid_len(val: str, min_val: int, max_val: int):
+    if len(val) < min_val or len(val) > max_val:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Поле должно быть в пределах от {min_val} до {max_val} символов"
+        )
+
+
+async def set_redis(name: str, value: dict, ex: int = 2):
+    value_str = json.dumps(value)
+    await redis.set(name=name, value=value_str, ex=ex)
+
+
+async def get_redis(key: str) -> dict | None:
+    data_dict = await redis.get(key)
+    if not data_dict:
+        return None
+    return json.loads(data_dict)
+
+
+def generate_code(code_len: int = 6) -> str:
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(letters_and_digits) for _ in range(code_len))
