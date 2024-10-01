8f442d438e57e6b4562969960145e7e3194e8bff
bot <maximBotPy@gmail.com>
2024-10-01 21:31:55 +0300

    refactor: заменил where на filter_by

--- back/core/crud.py
+++ back/core/crud.py
@@ -2,7 +2,7 @@ from core.mode import ModeRead
 from core.abs_model.model import CrudAbs
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, func, update, delete
+from sqlalchemy import select, update, delete
 from sqlalchemy.orm.decl_api import DeclarativeAttributeIntercept
 
 
@@ -16,28 +16,39 @@ class Crud(CrudAbs):
     @staticmethod
     async def read(
             session: AsyncSession, table: DeclarativeAttributeIntercept,
-            mode: ModeRead = ModeRead.ALL.value, **kwargs
+            mode: ModeRead = ModeRead.SEVERAL.value,
+            limit: int = 25,
+            **kwargs
     ) -> list:
         match mode:
             case ModeRead.ONE.value:
-                stmt = select(table).where(kwargs["field"] == kwargs["value"])
+                stmt = select(table).filter_by(**kwargs)
                 results = await session.execute(stmt)
                 return results.unique().scalar_one_or_none()
 
-            case ModeRead.ALL.value:
-                stmt = select(table)
+            case ModeRead.SEVERAL.value:
+                stmt = select(table).limit(limit).offset(1)
                 results = await session.execute(stmt)
                 return [item for items in results.all() for item in items]
 
     @staticmethod
-    async def update(session: AsyncSession, table: DeclarativeAttributeIntercept, field, field_val, data: dict):
-        query = update(table).where(func.lower(field) == func.lower(field_val)).values(**data)
+    async def update(
+            session: AsyncSession,
+            table: DeclarativeAttributeIntercept,
+            data: dict,
+            **kwargs
+    ):
+        query = update(table).filter_by(**kwargs).values(**data)
         await session.execute(query)
         await session.commit()
 
     @staticmethod
-    async def delite(session: AsyncSession, table: DeclarativeAttributeIntercept, field, field_val):
-        stmt = delete(table).where(func.lower(field) == func.lower(field_val))
+    async def delite(
+            session: AsyncSession,
+            table: DeclarativeAttributeIntercept,
+            **kwargs
+    ):
+        stmt = delete(table).filter_by(**kwargs)
         await session.execute(stmt)
         await session.commit()
 
