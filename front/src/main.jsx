7419b74890623704a1465f3fd7dc0a174869e8df
bot <maximBotPy@gmail.com>
2024-10-06 23:26:11 +0300

    feat: подключил стили для адаптива

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,6 +2,8 @@ import router from "./router"
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom"
 
+import "../media.sass"
+
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={ router } />
