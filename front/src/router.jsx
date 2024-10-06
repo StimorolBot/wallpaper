bc78f1516aafc96d88539c04feacd165588155ba
bot <maximBotPy@gmail.com>
2024-10-06 20:33:26 +0300

    feat: добавил путь для сброса пароля refactor: применил форматирование

--- front/src/router.jsx
+++ front/src/router.jsx
@@ -6,19 +6,57 @@ import { AboutImg } from "./page/AboutImg"
 import { CreateImg } from "./page/CreateImg"
 import { Register } from "./page/auth/Register"
 import { createBrowserRouter } from "react-router-dom"
+import { ResetPassword } from "./page/auth/ResetPassword"
 
 
 const router = createBrowserRouter([
-    { path: "/", element: <Home/>},
-    { path: "/create", element: <CreateImg/>},
-    { path: "/wallpaper/:uuid", element: <AboutImg/>},
-    { path: "/popular", element: <Popular/>},
-    { path: "/auth", children: [
-      { path: "login", element: <Login/>},
-      { path: "register", element: <Register/>},
-      { path: "", element: <Error errorCode={404} errorMsg={"Не далось найти страницу"}/>}
+    {
+      path: "/",
+      element: <Home/>
+    },
+    
+    {
+      path: "/create",
+      element: <CreateImg/>
+    },
+    
+    { 
+      path: "/wallpaper/:uuid", 
+      element: <AboutImg/>
+    },
+    
+    { 
+      path: "/popular",
+      element: <Popular/>
+    },
+    
+    { path: "/auth", 
+      children: [
+        { 
+          path: "login", 
+          element: <Login/>
+        },
+        
+        {
+          path: "register", 
+          element: <Register/>
+        },
+
+        {
+          path: "reset-password", 
+          element: <ResetPassword/>
+        },
+
+        { 
+          path: "", 
+          element: <Error/>
+        }
     ]},
-    { path: "*", element: <Error/>},
+
+    {
+      path: "*", 
+      element: <Error/>
+    },
   ]
 )
 
