0980f814193c3650ee604d78e5e3d17490d79444
bot <maximBotPy@gmail.com>
2024-10-13 12:25:39 +0300

    feat: добавил тест для сброса пароля

--- back/tests/test_auth.py
+++ back/tests/test_auth.py
@@ -4,18 +4,20 @@ from tests.conftest import ac
 from httpx import AsyncClient
 from core.help import set_redis, get_redis
 
+USER_EMAIL = "user@example.com"
+
 
 class TestAuthPos:
 
     async def test_get_code_confirm(self, ac: AsyncClient):
-        user_data = {"email": "user@example.com"}
+        user_data = {"email": USER_EMAIL}
         response = await ac.post("/auth/get-code", json=user_data)
         assert response.status_code == status.HTTP_200_OK
 
     async def test_register(self, ac: AsyncClient):
-        code = await get_redis(key="user@example.com")
+        code = await get_redis(key=USER_EMAIL)
         user_data = {
-            "email": "user@example.com",
+            "email": USER_EMAIL,
             "user_name": "string123",
             "password": "string123",
             "code_confirm": code["code"]
@@ -24,7 +26,7 @@ class TestAuthPos:
         assert response.status_code == status.HTTP_201_CREATED
 
     async def test_login(self, ac: AsyncClient):
-        user_data = {"email": "user@example.com", "password": "string123"}
+        user_data = {"email": USER_EMAIL, "password": "string123"}
         response = await ac.post("/auth/login", json=user_data)
 
         token = response.json()
@@ -35,7 +37,14 @@ class TestAuthPos:
         assert response.status_code == status.HTTP_200_OK
 
     async def test_reset_password(self, ac: AsyncClient):
-        ...
+        code = await get_redis(key=USER_EMAIL)
+        user_data = {
+            "email": USER_EMAIL,
+            "password": "new_password",
+            "code_confirm": code["code"]
+        }
+        response = await ac.patch("/auth/reset-password", json=user_data)
+        assert response.status_code == status.HTTP_200_OK
 
     async def test_logout(self, ac: AsyncClient):
         token = await get_redis("token_test")
