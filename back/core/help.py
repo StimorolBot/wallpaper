1696aef97cf4c1b16414edadfa403d8ac6c8106f
bot <maximBotPy@gmail.com>
2024-10-13 19:46:19 +0300

    feat: добавил функцию получения данных из заголовка

--- back/core/help.py
+++ back/core/help.py
@@ -2,8 +2,10 @@ import json
 import string
 import secrets
 from uuid import uuid4
+from fastapi import status, HTTPException, Request
+
 from src.conf import redis
-from fastapi import status, HTTPException
+from core.logger import auth_logger
 
 
 def generate_uuid() -> str:
@@ -56,3 +58,17 @@ async def get_redis(key: str) -> dict | None:
 def generate_code(code_len: int = 6) -> str:
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(secrets.choice(letters_and_digits) for _ in range(code_len))
+
+
+def get_info_from_headers(request: Request) -> list:
+    try:
+        user_agent = request.headers["user-agent"]
+        origin = request.headers["origin"]
+        client_host = request.client.host
+        return [user_agent, origin, client_host]
+    except KeyError:
+        auth_logger.error("Не удалось получить данные из headers")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Не удалось получить данные из headers"
+        )
