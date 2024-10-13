ebe66c46b975c65b94c37dd34fc2e7c5ecd2bffa
bot <maximBotPy@gmail.com>
2024-10-13 19:28:28 +0300

    refactor: переписал запросы на httpx feat: добавил обработку исключений при отсутствии ключа uuid

--- back/src/app/img/generate_img.py
+++ back/src/app/img/generate_img.py
@@ -1,28 +1,31 @@
 import json
-import aiohttp
 import requests
 from asyncio import sleep
+from httpx import AsyncClient
+from fastapi import status, HTTPException
 
+from core.logger import img_logger
 from src.app.img.config import settings
-from src.app.img.style_img import StyleImg
+from src.app.img.enums.style_img import StyleImg
 
 
 class Text2ImageAPI:
 
     def __init__(self, url, api_key, secret_key):
         self.url = url
-        self.auth_headers = {'X-Key': f'Key {api_key}', 'X-Secret': f'Secret {secret_key}'}
+        self.auth_headers = {"X-Key": f"Key {api_key}", "X-Secret": f"Secret {secret_key}"}
 
     async def get_model(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self.url + 'key/api/v1/models', headers=self.auth_headers) as response:
-                data = await response.json()
-                return data[0]['id']
+        async with AsyncClient() as client:
+            response = await client.get(f"{self.url}key/api/v1/models", headers=self.auth_headers)
+            data = response.json()
+            return data[0]['id']
 
     async def generate(
             self, prompt: str, model: int, style: StyleImg,
             width: int, height: int, images: int = 1
     ):
+        # переписать на httpx или aiohttp
         params = {
             "type": "GENERATE",
             "style": style,
@@ -38,29 +41,32 @@ class Text2ImageAPI:
             'model_id': (None, model),
             'params': (None, json.dumps(params), 'application/json')
         }
-        response = requests.post(self.url + 'key/api/v1/text2image/run', headers=self.auth_headers, files=payload)
-        data = response.json()
+        response = requests.post(
+            f"{self.url}key/api/v1/text2image/run",
+            headers=self.auth_headers, files=payload
+        )
 
-        #async with aiohttp.ClientSession() as session:
-        #   async with session.post(
-        #          self.url + 'key/api/v1/text2image/run',
-        #         headers=self.auth_headers, files=payload
-        #) as response:
-        #   data = await response.json()
-        #  print(data)
-        # return data["uuid"]
-        return data["uuid"]
+        data = response.json()
+        try:
+            return data["uuid"]
+        except KeyError:
+            img_logger.error("Не удалось сгенерировать изображение.\n Response: %s", data)
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Не удалось сгенерировать изображение, пожалуйста повторите попытку позже"
+            )
 
     async def check_generation(self, request_id, attempts: int = 10, delay: int = 10):
         while attempts > 0:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                        f"{self.url}key/api/v1/text2image/status/{request_id}", headers=self.auth_headers
-                ) as response:
-                    data = await response.json()
+            async with AsyncClient() as client:
+                response = await client.get(
+                    f"{self.url}key/api/v1/text2image/status/{request_id}",
+                    headers=self.auth_headers
+                )
+                data = response.json()
 
-                    if data['status'] == 'DONE':
-                        return data['images']
+                if data['status'] == 'DONE':
+                    return data['images']
 
             attempts -= 1
             await sleep(delay)
