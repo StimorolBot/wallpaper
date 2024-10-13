ad6d693e98276ed863a3d506b7051029470b050f
bot <maximBotPy@gmail.com>
2024-10-13 19:30:46 +0300

    feat: добавил conftest для запуска test_img

---
+++ back/tests/test_img/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from httpx import AsyncClient
+
+from core.crud import crud
+from core.help import set_redis
+
+from src.app.auth.model import AuthTable
+from src.app.auth.password_auth import password_auth
+
+from tests.conftest import async_session_maker
+from tests.conftest import ac, TEST_USER_EMAIL
+
+
+@pytest.fixture(autouse=True, scope="session")
+async def add_record_in_database():
+    data = {
+        "email": TEST_USER_EMAIL,
+        "user_name": "username",
+        "hash_password": password_auth.get_hash_password("password123")
+    }
+    async with async_session_maker() as session:
+        await crud.create(session=session, table=AuthTable, data=data)
+
+
+@pytest.fixture(autouse=True, scope="class")
+async def login(ac: AsyncClient):
+    user_data = {"email": TEST_USER_EMAIL, "password": "password123"}
+    response = await ac.post("/auth/login", json=user_data)
+
+    token = response.json()
+    await set_redis(
+        name="img_token_test",
+        value={"access_token": token["access_token"]}
+    )
