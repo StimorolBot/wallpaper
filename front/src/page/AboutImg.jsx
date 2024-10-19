cfc88e87e922c955d9bc15b5bb8ab70ba07e1afa
bot <maximBotPy@gmail.com>
2024-10-20 01:10:47 +0300

    refactor: перенес создание карточек с информацией о картинке в отдельный компонент

--- front/src/page/AboutImg.jsx
+++ front/src/page/AboutImg.jsx
@@ -1,94 +1,59 @@
-import { api } from "../api"
 import { useEffect, useState } from "react"
+
+import { api } from "../api/config"
+import { Loader } from "../components/loader/Loader"
 import { Header } from "../components/header/Header"
+import { Footer } from "../components/footer/Footer"
+import { useFetch } from "../components/hook/useFetch"
+import { CreateImgList } from "../components/img/CreateImgList"
+import { AboutImgCard } from "../components/ui/cards/AboutImgCard"
 
 import "./style/about_img.sass"
 
 
 export function AboutImg(){
-    const [imgInfo, setImgInfo] = useState("")
+    const [imgInfo, setImgInfo] = useState()
     const uuidImg = window.location.pathname.split("wallpaper/")[1]
     
-    const setReaction = async (event) => {
-        event.preventDefault()
-    // вынести в отдельную ф. 
-    }
-  
-    
+    const [request, isLoading, error] = useFetch(
+        async () => {
+            await api.get(`/wallpaper/${uuidImg}`)
+                .then((response) => {
+                    setImgInfo([response.data])
+                }
+            )
+        }
+    )
+
     useEffect(() => {(
         async () => {
-            await api.get(`/wallpaper/${uuidImg}`).then((response) => {
-                setImgInfo({...response.data})
-            }).catch((error) =>{
-                console.log(error)
-            })
+            await request()
         })()
     }, [])
 
     return(
         <>
         <Header/>
-        <section className="about-img section_flex">
+        <section className="about-img-section">
             <h2 className="hidden">
                 Информация о изображении
             </h2>
             <div className="wrapper">
                 <div className="about-img__container">
-                    <div className="about__img-container">
-                        <img className="about__img"
-                            src={"data:image/jpeg;base64," + imgInfo["img_base64"]} alt="img"
-                        />
-                    </div>
-                    <div className="about__list">
-                        <div className="about__list-container">
-                            <dt className="about__list-dt">Автор:</dt>
-                            <dd className="about__list-dd">{imgInfo["user_name"]}</dd>
-                        </div>
-
-                        <div className="about__list-container">
-                            <dt className="about__list-dt">Стиль:</dt>
-                            <dd className="about__list-dd">
-                                {imgInfo["style"]}
-                            </dd>
-                        </div>
-
-                        <div className="about__list-container">
-                            <dt className="about__list-dt">Дата создания:</dt>
-                            <dd className="about__list-dd">
-                                <time dateTime={imgInfo["create_date"]}>
-                                    {imgInfo["create_date"]?.split("T")[0]}
-                                </time>
-                            </dd>
-                        </div>
-
-                        <div className="about__list-container">
-                            <dt className="about__list-dt">Промт:</dt>
-                            <dd className="about__list-dd about__list-promt">
-                                {imgInfo["prompt"]}
-                            </dd>
-                        </div>
-
-                        <div className="reaction__container">
-                            <div className="reaction__item">
-                                <img className="reaction-img" src="/static/like.svg" alt="like" />
-                                <p className="reaction-count">{imgInfo["like"]}</p>
+                    {isLoading
+                        ? <Loader/>
+                        :
+                        <div className="about-img__wrapper">
+                            <div className="about__img">
+                                <CreateImgList imgList={imgInfo} setImgList={setImgInfo} flag={false}/>
                             </div>
-
-                            <div className="reaction__item">
-                                <img className="reaction-img" src="/static/dislike.svg" alt="dislike" />
-                                <p className="reaction-count">{imgInfo["dislike"]}</p>
-                            </div>        
+                            <AboutImgCard imgInfo={ imgInfo } uuidImg={ uuidImg }/>
                         </div>
-                        <div className="download-img__container">
-                            <a className="download-img-link hover" download={`${uuidImg}.png`}
-                                href={"data:image/jpeg;base64," + imgInfo["img_base64"]}>
-                                Скачать
-                            </a>
-                        </div> 
-                    </div>
+                    }
                 </div>
             </div>
         </section>
+        <Footer/>
         </>
     )
 }
