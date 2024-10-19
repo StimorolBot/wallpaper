694ece0634a2df8527686338bb03357c03bd49bd
bot <maximBotPy@gmail.com>
2024-10-19 19:30:29 +0300

    refactor: переименовал имя картинки

--- front/src/components/loader/Loader.jsx
+++ front/src/components/loader/Loader.jsx
@@ -3,7 +3,7 @@ import "./style/loader.sass"
 export function Loader(){
     return(
         <div className="loader">
-            <img className="loader-img" src="/static/gear.svg" alt="loader" />
+            <img className="loader-img" src="/static/loader.svg" alt="loader" />
             <p className="loader-text">
                 Пожалуйста подождите, идет загрузка...
             </p>
