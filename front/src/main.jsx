7cca26c4942b40d91c530ec49a18194087a334e8
bot <maximBotPy@gmail.com>
2024-10-20 01:17:08 +0300

    refactor: изменил пути импортов

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,7 +2,7 @@ import router from "./router"
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom"
 
-import "../media.sass"
+import "./media.sass"
 
 
 createRoot(document.getElementById('root')).render(
