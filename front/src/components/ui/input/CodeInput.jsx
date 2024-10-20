c01f0dc79ca624995137a78af1a19d786e1af72a
bot <maximBotPy@gmail.com>
2024-10-20 17:14:51 +0300

    feat: добавил компонент для получения кода подтверждения

---
+++ front/src/components/ui/input/CodeInput.jsx
@@ -0,0 +1,37 @@
+import { AuthInput } from "./AuthInput"
+import { useFetch } from "../../hook/useFetch"
+
+
+export function CodeInput({authData}){
+    
+    const [getCode, isLoadingCode, errorCode] = useFetch(
+        async (event) => {
+            event.preventDefault()
+            if (authData["email"] !== null){
+                await api.post("/auth/get-code",
+                    {"email_type": "CONFIRM", "email": authData["email"]},
+                    {headers:{}}
+                )
+                .then((response) => {
+                    console.log(response.data)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            }
+        }
+    )
+
+    return(
+        <AuthInput
+            id={"code-confirm-input"}
+            lblTitle={"Код подтверждения"}
+            type={"text"}
+            minLength={6}
+            maxLength={6}
+            inputType={"code"}
+            getCode={getCode}
+            onChange={(event) => setAuthData({...authData, "code_confirm": event.target.value})}
+        />
+    )
+}
