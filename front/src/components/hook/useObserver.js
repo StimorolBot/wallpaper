4bec44703e731226a8d856e25a12c5645f54a929
bot <maximBotPy@gmail.com>
2024-10-23 00:10:23 +0300

    feat: добавил зависимость в useEffect и состояния для текущей страницы и общего их количества

--- front/src/components/hook/useObserver.js
+++ front/src/components/hook/useObserver.js
@@ -1,12 +1,12 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
-export const useObserver = async (
-    isLoading, pageRef,
-    pagesRef, request,
-    lastElementRef, itemList
-) => {
+export const useObserver = async (isLoading, request, lastElementRef, itemList, filter) => {
     const observerRef = useRef(null)
+    const prevFilterState = useRef(undefined)
+    
+    const [currentPage, setCurrentPage] = useState(1)
+    const [totalPage, setTotalPage] = useState(1)
 
     useEffect(() => {
         if (isLoading)
@@ -14,16 +14,17 @@ export const useObserver = async (
     
         if (observerRef.current)
             observerRef.current.disconnect()
-    
+
         const callback = async (entries, observer) => {
-            if (entries[0].isIntersecting && pageRef.current <= pagesRef.current){
-                await request()
+            if (prevFilterState.current !== filter || entries[0].isIntersecting && currentPage <= totalPage){
+                await request(currentPage, setCurrentPage, setTotalPage)
+                prevFilterState.current = filter
             }
         }
-    
+        
         observerRef.current = new IntersectionObserver(callback)
         observerRef.current.observe(lastElementRef.current)
         
-    }, [itemList])
+    }, [itemList, filter])
     
 }
