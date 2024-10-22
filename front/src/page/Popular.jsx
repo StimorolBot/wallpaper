64614e445778fac69f528725d559357db86a9ebe
bot <maximBotPy@gmail.com>
2024-10-22 23:59:39 +0300

    feat: добавил футтер, пагинацию, значения для выпадающего меню

--- front/src/page/Popular.jsx
+++ front/src/page/Popular.jsx
@@ -1,36 +1,42 @@
-import api from "../api"
-import { useEffect, useState } from "react"
-import { Header } from "/src/components/header/Header"
+import { useState } from "react"
 
-
-import "./style/popular.sass"
+import { Header } from "../components/header/Header"
+import { Footer } from "../components/footer/Footer"
+import { Pagination } from "../components/pagination/Pagination"
 import { DropDownMenu } from "../components/ui/menu/DropDownMenu"
 
 
-export function Popular(){
+export function Popular() {
+    const [filterTime, setFilterTime] = useState("DAY")
     const [imgList, setImgList] = useState([])
     
-    useEffect(() => {(
-        async () => {
-            await api.get("/popular").then((response) => {            
-                setImgList([...response.data])
-            }).catch((error) =>{
-                console.log(error)
-            })
-        })()
-    }, [])
-
     return(
         <>
         <Header/>
-        <section className="popular">
-            <h2 className="hidden">
-                Популярное
-            </h2>
-            <div className="wrapper">
-                <DropDownMenu/>
+        <section className="popular flex">
+            <h2 className="hidden">Популярное</h2>
+            <div className="wrapper">                
+                <DropDownMenu
+                    title={"Популярное за..."}
+                    setFilter={setFilterTime}
+                    itemList={[
+                        {"front": "Час", "back": "HOUR"},
+                        {"front": "День", "back": "DAY"},
+                        {"front": "Неделю", "back": "WEEK"},
+                        {"front": "Месяц", "back": "MONTH"},
+                        {"front": "Год", "back": "YEAR"},
+                        {"front": "Все время", "back": "ALL"}
+                    ]}
+                />
+                <Pagination
+                    itemList={imgList}
+                    setItemList={setImgList}
+                    path={"/popular"}
+                    params={{"filter_time": filterTime}}
+                />
             </div>
         </section>
-        </>  
-    )
+        <Footer/>
+    </>
+  )
 }
