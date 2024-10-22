ea9eadf4402bd4dc301b3bffa3ab4cf9a0b4ea43
bot <maximBotPy@gmail.com>
2024-10-22 19:25:48 +0300

    refactor: переименовал картинки, классы

--- front/src/components/ui/input/AuthInput.jsx
+++ front/src/components/ui/input/AuthInput.jsx
@@ -3,17 +3,18 @@ import { useState } from "react"
 import "./style/auth_input.sass"
 
 
-export function AuthInput({id, lblTitle, inputType, emailConfirm, ...props}){
+export function AuthInput({id, lblTitle, inputType, getCode, ...props}){
     const [isShow, setIsShow] = useState(false)
     const [isActive, setIsActive] = useState(false)
 
     return(
         <div className="auth-input__item">
-            <input className={isActive ? "auth-input_active" : "auth-input"}
+            <input className={isActive ? "auth-input auth-input_active" : "auth-input"}
                 id={ id }
                 autoComplete="off"
                 type={isShow === false ? "password" : "text"}
                 required
+                onClick={() => setIsActive(state => !state)}
                 { ...props }
             />
 
@@ -21,7 +22,7 @@ export function AuthInput({id, lblTitle, inputType, emailConfirm, ...props}){
                 inputType == "password" &&
                     <img
                         className={isActive === true ? "password-show password-show_active" : "password-show"} 
-                        src={isShow === false ? "/static/password-show.svg" : "/static/password-see.svg"} 
+                        src={isShow === false ? "/static/psd-show.svg" : "/static/psd-hidden.svg"} 
                         alt="password-show"
                         onClick={() => setIsShow((state) => !state)}                        
                     />
@@ -33,15 +34,15 @@ export function AuthInput({id, lblTitle, inputType, emailConfirm, ...props}){
                         className={isActive === true ? "code-input-img code-input-img_active" : "code-input-img"}
                         src="/static/enter.svg" 
                         alt="enter"
-                        onClick={(event) => emailConfirm(event)}
+                        onClick={(event) => getCode(event)}
                         title="Отправить код подтверждения на почту"
                     />
             }
 
-            <label className={isActive ? "auth-lbl_active" : "auth-lbl"} htmlFor={ id }
-                onClick={() => setIsActive(state => !state)}>
+            <label className={isActive ? "auth-lbl auth-lbl_active" : "auth-lbl"} htmlFor={ id }>
                 { lblTitle }
             </label>
+            <span className="auth-border"></span>
         </div>
     )
 }
