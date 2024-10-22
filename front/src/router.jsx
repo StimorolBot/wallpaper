e1c17ff1cbb7c68c8387a311db9984dee9b4000c
bot <maximBotPy@gmail.com>
2024-10-22 15:33:16 +0300

    refactor: исправил отступы

--- front/src/router.jsx
+++ front/src/router.jsx
@@ -1,3 +1,5 @@
+import { createBrowserRouter } from "react-router-dom"
+
 import { Home } from "./page/Home"
 import { Error } from "./page/Error"
 import { Popular } from "./page/Popular"
@@ -5,60 +7,58 @@ import { Login } from "./page/auth/Login"
 import { AboutImg } from "./page/AboutImg"
 import { CreateImg } from "./page/CreateImg"
 import { Register } from "./page/auth/Register"
-import { createBrowserRouter } from "react-router-dom"
 import { ResetPassword } from "./page/auth/ResetPassword"
 
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Home/>
+        path: "/",
+        element: <Home/>
     },
-    
+
     {
-      path: "/create",
-      element: <CreateImg/>
+        path: "/create",
+        element: <CreateImg/>
     },
-    
-    { 
-      path: "/wallpaper/:uuid", 
-      element: <AboutImg/>
+
+    {
+        path: "/wallpaper/:uuid",
+        element: <AboutImg/>
     },
-    
-    { 
-      path: "/popular",
-      element: <Popular/>
+
+    {
+        path: "/popular",
+        element: <Popular/>
     },
-    
-    { path: "/auth", 
-      children: [
-        { 
-          path: "login", 
-          element: <Login/>
-        },
-        
+
+    {
+        path: "/auth",
+        children: [
+            {
+                path: "login",
+                element: <Login/>
+            },
+            
         {
-          path: "register", 
-          element: <Register/>
+            path: "register",
+            element: <Register/>
         },
 
         {
-          path: "reset-password", 
-          element: <ResetPassword/>
+            path: "reset-password",
+            element: <ResetPassword/>
         },
 
-        { 
-          path: "", 
-          element: <Error/>
+        {
+            path: "",
+            element: <Error/>
         }
     ]},
 
     {
-      path: "*", 
-      element: <Error/>
+        path: "*",
+        element: <Error/>,
     },
-  ]
-)
-
+])
 
 export default router
