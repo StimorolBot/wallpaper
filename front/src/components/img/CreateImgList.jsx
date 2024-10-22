336d2f8d63aef1b2e8c8433901d07ffc5d8b4d1a
bot <maximBotPy@gmail.com>
2024-10-22 15:19:59 +0300

    feat: добавил флаг для отслеживания последнего элемента

--- front/src/components/img/CreateImgList.jsx
+++ front/src/components/img/CreateImgList.jsx
@@ -7,7 +7,7 @@ import { BtnDownload } from "../ui/btn/BtnDownload"
 import "./style/create_img_list.sass"
 
 
-export function CreateImgList({imgList, setImgList, lastElementRef}){
+export function CreateImgList({imgList, setImgList, lastElementRef, flag=true}){
 
     const [setReaction, isLoading, error] = useFetch(
         async (event, reaction, item, index) => {
@@ -26,42 +26,38 @@ export function CreateImgList({imgList, setImgList, lastElementRef}){
     )
         
     return(
-        <div className="img__container">
-            { imgList.map(
-                (item, index) => {
-                    return(
-                        <div className="img__wrapper" key={item["uuid_img"]}>
-                            <BtnDownload
-                                href={"data:image/jpeg;base64," + item["img_base64"]}
-                                name={item["uuid_img"]}
-                            />
-                            <Link className="img__link" to={`/wallpaper/${item["uuid_img"]}`}>
-                                <img 
-                                    className="img__item"
-                                    alt="img-ai"
-                                    name={item["uuid_img"]}
-                                    loading="lazy"
-                                    src={"data:image/jpeg;base64," + item["img_base64"]}
-                                />
-                            </Link>
-                            <div className="reaction-img__hov">
-                                <img 
-                                    className={item.reaction == true ? "like like_active": "like"}
-                                    src="/static/like.svg" alt="like"
-                                    onClick={(event) => setReaction(event, true, item, index)}
-                                />
-                                
-                                <img
-                                    className={item.reaction == false ? "dislike dislike_active": "dislike"}
-                                    src="/static/dislike.svg" alt="dislike"
-                                    onClick={(event) => setReaction(event, false, item, index)}
-                                />
-                            </div>
-                        </div>
-                    )
-                }
-            )}
-        <span ref={lastElementRef}></span>
-        </div>
+        <>
+        { imgList?.map((item, index) => {
+            return(
+                <div className="img__wrapper" key={index}>
+                    <BtnDownload
+                        href={`data:image/jpeg;base64,${item["img_base64"]}`}
+                        name={item["uuid_img"]}
+                    />
+                    <Link className="img__link" to={`/wallpaper/${item["uuid_img"]}`}>
+                        <img 
+                            className={flag ? "img__item" : "img__item img__item_solo"}
+                            alt="img-ai"
+                            name={item["uuid_img"]}
+                            loading="lazy"
+                            src={"data:image/jpeg;base64," + item["img_base64"]}
+                        />
+                    </Link>
+                    <div className="reaction-img__hov">
+                        <img 
+                            className={item.reaction == true ? "like like_active": "like"}
+                            src="/static/like.svg" alt="like"
+                            onClick={(event) => setReaction(event, true, item, index)}
+                        />
+                        <img
+                            className={item.reaction == false ? "dislike dislike_active": "dislike"}
+                            src="/static/dislike.svg" alt="dislike"
+                            onClick={(event) => setReaction(event, false, item, index)}
+                        />
+                    </div>
+                </div>
+            )})}
+        { flag && <span ref={lastElementRef}></span> }
+        </>
     )
 }
