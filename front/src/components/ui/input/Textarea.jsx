0c712ef1b36924f60f2610f792deb98e6c6950e4
bot <maximbotpy@gmail.com>
2024-11-02 17:00:00 +0300

    frat: добавил автоматическое увеличение высоты

--- front/src/components/ui/input/Textarea.jsx
+++ front/src/components/ui/input/Textarea.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useRef } from "react"
+
 import "./style/textarea.sass"
 
 
-export function Textarea({placeholder, ...props}){
+export function Textarea({placeholder, value, setValue, ...props}){
+    const textAreaRef = useRef(null)
+
+    useEffect(
+        () => {
+            textAreaRef.current.style.height = "auto"
+            textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px"
+        }, [value]
+    )
+
     return(
-        <textarea className="prompt-textarea" placeholder={placeholder} required {...props}></textarea>
+        <textarea
+            className="prompt-textarea"
+            placeholder={placeholder}
+            ref={textAreaRef}
+            required
+            onChange={(event) => setValue({...value, prompt: event.target.value})}
+            {...props}>
+        </textarea>
     )
 }
