b3a0b440dfba8ff037c22f762d567b64ac276c63
bot <maximbotpy@gmail.com>
2024-11-02 17:03:36 +0300

    feat: добавил пропс для выравнивания

--- front/src/components/loader/Loader.jsx
+++ front/src/components/loader/Loader.jsx
@@ -1,8 +1,8 @@
 import "./style/loader.sass"
 
-export function Loader(){
+export function Loader({align = true}){
     return(
-        <div className="loader">
+        <div className={align ? "loader loader_align" : "loader"}>
             <img className="loader-img" src="/static/loader.svg" alt="loader" />
             <p className="loader-text">
                 Пожалуйста подождите, идет загрузка...
