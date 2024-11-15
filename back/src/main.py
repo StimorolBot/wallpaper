d455bf2eb25ff81b38f417fd9202ae64a44150b6
bot <maximbotpy@gmail.com>
2024-11-16 00:12:50 +0300

    feat: добавил адрес для обращения в бэку

--- back/src/main.py
+++ back/src/main.py
@@ -27,7 +27,7 @@ app.include_router(img_router)
 app.include_router(register_router)
 app.include_router(jwt_router)
 
-origins = ["http://localhost:5173"]
+origins = ["http://localhost:5173", "http://127.0.0.1:80"]
 
 app.add_middleware(
     CORSMiddleware,
