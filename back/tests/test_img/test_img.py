4ab427506b18c5bae43a1b8d472b919074349b11
bot <maximbotpy@gmail.com>
2024-11-16 00:10:41 +0300

    feat: добавил тесты для получения картинки по uuid, создания реакции, запросы без кук

--- back/tests/test_img/test_img.py
+++ back/tests/test_img/test_img.py
@@ -4,6 +4,7 @@ from httpx import AsyncClient
 
 from tests.conftest import ac
 from core.my_functools import get_redis
+
 from src.app.img.enums.style_img import StyleImg
 from src.app.img.enums.filter_time import FilterTime
 
@@ -15,91 +16,121 @@ class TestImgPos:
         response = await ac.get("/", cookies={"access_token": token["access_token"]})
         assert response.status_code == status.HTTP_200_OK
 
-    async def test_get_home_page_no_cookies(self, ac: AsyncClient):
+    @pytest.mark.parametrize(
+        "filter_time",
+        (
+                FilterTime.HOUR.value, FilterTime.DAY.value,
+                FilterTime.WEEK.value, FilterTime.MONTH.value,
+                FilterTime.YEAR.value, FilterTime.ALL.value
+        )
+    )
+    async def test_get_popular_img(self, ac: AsyncClient, filter_time: str):
+        token = await get_redis("img_token_test")
+        response = await ac.get(
+            "/popular",
+            params={"filter_time": filter_time},
+            cookies={"access_token": token["access_token"]}
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_get_img_by_uuid(self, ac: AsyncClient):
+        token = await get_redis("img_token_test")
+        response = await ac.get(
+            "/wallpaper/a4c687d2dde54c1aa39385d23f2fcc9d",
+            params={"uuid_img": "a4c687d2dde54c1aa39385d23f2fcc9d"},
+            cookies={"access_token": token["access_token"]}
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+    @pytest.mark.parametrize("reaction", (True, False))
+    async def test_set_reaction(self, ac: AsyncClient, reaction):
+        data = {"reaction": reaction, "img_uuid": "a4c687d2dde54c1aa39385d23f2fcc9d"}
+        token = await get_redis("img_token_test")
+        response = await ac.post(
+            "set-reaction",
+            json=data,
+            cookies={"access_token": token["access_token"]}
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_create_img(self, ac: AsyncClient):
+        token = await get_redis("img_token_test")
+        data = {"prompt": "тест", "style": StyleImg.ANIME.value}
+        response = await ac.post("/create", json=data, cookies={"access_token": token["access_token"]})
+        assert response.status_code == status.HTTP_201_CREATED
+
+
+class TestImgNeg:
+
+    @pytest.mark.parametrize("path", ("/test", "/auth/test", "/popular/test"))
+    async def test_page(self, ac: AsyncClient, path: str):
+        response = await ac.get(url=path)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    @pytest.mark.parametrize(
+        "prompt, style, width, height",
+        [
+            (" ", "ANIME", 1024, 1024),
+            ("test1", "CYBERPUNK", 1024, 1024),
+            ("test1", " ", 1024, 1024),
+            ("test1", "ANIME", -1024, 1024),
+            ("test1", "ANIME", 1024, -1024),
+            ("test1", "ANIME", 543, 1024),
+            ("test1", "ANIME", 1024, 543),
+            ("test1", "ANIME", 2000, 1024),
+            ("test1", "ANIME", 1024, 2000),
+            ("test ! @ + - / . | \\", "ANIME", 1024, 1024)
+        ]
+    )
+    async def test_create_img(self, ac: AsyncClient, prompt: str, style: str, width: int, height: int):
+        token = await get_redis("img_token_test")
+        data = {"prompt": prompt, "style": style, "width": width, "height": height}
+        response = await ac.post("/create", json=data, cookies={"access_token": token["access_token"]})
+
+        if response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY:
+            assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        else:
+            assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    @pytest.mark.parametrize("filter_time", (" ", "ANY"))
+    async def test_get_popular_img(self, ac: AsyncClient, filter_time: str):
+        response = await ac.get("/popular", params={"filter_time": filter_time})
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+class TestImgPosNoCookies:
+
+    async def test_get_home_page(self, ac: AsyncClient):
         response = await ac.get("/")
         assert response.status_code == status.HTTP_200_OK
 
     @pytest.mark.parametrize(
         "filter_time",
-        (FilterTime.HOUR.value, FilterTime.DAY.value,
-         FilterTime.WEEK.value, FilterTime.MONTH.value,
-         FilterTime.YEAR.value, FilterTime.ALL.value
-         )
+        (
+                FilterTime.HOUR.value, FilterTime.DAY.value,
+                FilterTime.WEEK.value, FilterTime.MONTH.value,
+                FilterTime.YEAR.value, FilterTime.ALL.value
+        )
     )
-    async def test_get_popular_img(self, ac: AsyncClient, filter_time: str):
+    async def test_get_popular_img_no_cookies(self, ac: AsyncClient, filter_time: str):
         response = await ac.get("/popular", params={"filter_time": filter_time})
         assert response.status_code == status.HTTP_200_OK
 
-    async def test_create_img(self, ac: AsyncClient):
-        token = await get_redis("img_token_test")
-        data = {
-            "prompt": "тест",
-            "style": StyleImg.ANIME.value,
-            "width": 1000,
-            "height": 500
-        }
-
-        response = await ac.post("/create", json=data, cookies={"access_token": token["access_token"]})
-        assert response.status_code == status.HTTP_201_CREATED
-
     async def test_get_img_by_uuid(self, ac: AsyncClient):
-        ...
+        response = await ac.get(
+            "/wallpaper/a4c687d2dde54c1aa39385d23f2fcc9d",
+            params={"uuid_img": "a4c687d2dde54c1aa39385d23f2fcc9d"})
+        assert response.status_code == status.HTTP_200_OK
 
-    async def test_set_reaction(self, ac: AsyncClient):
-        ...
 
-
-class TestImgNeg:
-    @pytest.mark.parametrize("path", ("/test", "/auth/test", "/popular/test"))
-    async def test_page_not_found(self, ac: AsyncClient, path: str):
-        response = await ac.get(url=path)
-        assert response.status_code == status.HTTP_404_NOT_FOUND
+class TestImgNegNoCookies:
 
     async def test_create_img_no_cookies(self, ac: AsyncClient):
-        data = {
-            "prompt": "тест",
-            "style": StyleImg.ANIME.value,
-            "width": 1000,
-            "height": 500
-        }
+        data = {"prompt": "тест1", "style": StyleImg.ANIME.value}
         response = await ac.post("/create", json=data)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    @pytest.mark.parametrize(
-        "prompt, style, width, height",
-        [(" ", "ANIME", 200, 200),
-         ("test", " ", 200, 200),
-
-         ("test", "ANIME", -200, 200),
-         ("test", "ANIME", 200, -200),
-
-         ("test", "ANIME", 2000, 200),
-         ("test", "ANIME", 200, 2000),
-
-         ("p", "ANIME", 200, 200),
-         ("test", "TEST_STYLE", 200, 200),
-         ("test ! @ + - / . | \\", "ANIME", 200, 200)]
-    )
-    async def test_create_img_bad_request(
-            self, ac: AsyncClient,
-            prompt: str, style: str,
-            width: int, height: int
-    ):
-        token = await get_redis("img_token_test")
-        data = {
-            "prompt": prompt,
-            "style": style,
-            "width": width,
-            "height": height
-        }
-
-        response = await ac.post("/create", json=data, cookies={"access_token": token["access_token"]})
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-    @pytest.mark.parametrize("filter_time", (" ", "ANY"))
-    async def test_get__bad_popular_img(self, ac: AsyncClient, filter_time: str):
-        response = await ac.get("/popular", params={"filter_time": filter_time})
-        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-
-
-
+    async def test_set_reaction(self, ac: AsyncClient):
+        data = {"reaction": True, "img_uuid": "a4c687d2dde54c1aa39385d23f2fcc9d"}
+        response = await ac.post("set-reaction", json=data)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
