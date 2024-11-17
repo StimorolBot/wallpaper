95f3eb18d7021530405211e5a70bf51da35de39a
bot <maximbotpy@gmail.com>
2024-11-17 23:53:35 +0300

    refactor: убрал проверки для вывода кода, добавил пропсам значения по умолчанию

--- front/src/page/Error.jsx
+++ front/src/page/Error.jsx
@@ -3,27 +3,23 @@ import { Link } from "react-router-dom"
 import "./style/error.sass"
 
 
-export function Error({errorCode, errorMsg}) {
-    const url = window.location.href
-
+export function Error({
+    errorCode = 404,
+    errorMsg=`Не удалось найти страницу: ${window.location.href}`})
+{
     return(
         <section className="error__container">
             <h2 className="error__code">
-            { errorCode == undefined
-                ? "404"
-                : errorCode
-                }
-                
+                { errorCode }
             </h2>
             <div className="error__msg">
-                { errorMsg == undefined
-                ? `Не удалось найти страницу: ${url}`
-                : errorMsg
-                }
+                { errorMsg }
             </div>
-            <Link className="back-home" to="/">
-                Вернуться на главную страницу 
-            </Link>
+            { errorCode === 404 &&
+                <Link className="back-home" to="/">
+                    Вернуться на главную страницу 
+                </Link>
+            }
         </section>
     )
 }
