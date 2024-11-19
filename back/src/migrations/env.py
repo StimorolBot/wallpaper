f5400d026c269d62c1f2c07f6b225a89a223983f
bot <maximbotpy@gmail.com>
2024-11-20 02:14:01 +0300

    refactor: переименовал атрибуты

--- back/src/migrations/env.py
+++ back/src/migrations/env.py
@@ -16,11 +16,11 @@ from core.model import Base
 config = context.config
 section = config.config_ini_section
 
-config.set_section_option(section, "DB_HOST", setting.DB_HOST)
-config.set_section_option(section, "DB_NAME", setting.DB_NAME)
-config.set_section_option(section, "DB_USER", setting.DB_USER)
-config.set_section_option(section, "DB_PASS", setting.DB_PASS)
-config.set_section_option(section, "DB_PORT", setting.DB_PORT)
+config.set_section_option(section, "DB_HOST", setting.POSTGRES_HOST)
+config.set_section_option(section, "DB_NAME", setting.POSTGRES_DB)
+config.set_section_option(section, "DB_USER", setting.POSTGRES_USER)
+config.set_section_option(section, "DB_PASS", setting.POSTGRES_PASSWORD)
+config.set_section_option(section, "DB_PORT", setting.POSTGRES_PORT)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -31,7 +31,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 target_metadata = Base.metadata
-#target_metadata = None
+
+
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
