d64169c08579ba395a233bc2ac02215e3d76dfea
bot <maximbotpy@gmail.com>
2024-11-20 00:22:04 +0300

    feat: добавил конфигурацию для redis

--- back/src/config.py
+++ back/src/config.py
@@ -1,6 +1,16 @@
 from redis import asyncio as aioredis
+from pydantic_settings import BaseSettings
 
-redis = aioredis.from_url(
-    "redis://localhost:6379", encoding="utf-8",
-    decode_responses=True
-)
+class Settings(BaseSettings):
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_USER_NAME: str = "user_name"
+    REDIS_PASSWORD: str = "password"
+
+    @property
+    def get_url(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
+
+settings = Settings()
+
+redis = aioredis.from_url(settings.get_url, encoding="utf-8", decode_responses=True) # передать psd и name
