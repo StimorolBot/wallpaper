50dd297dd79202d74a526f26a4dd3b0e1509ae06
bot <maximbotpy@gmail.com>
2024-11-25 23:09:25 +0300

    feat: добавил дополнительное меню, поиск изменил логотип

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,40 +1,44 @@
 import cookies from "../../cookie"
 import { Link } from "react-router-dom"
+import { SubMenu } from "../ui/menu/SubMenu"
+import { MainSearch } from "../ui/input/MainSearch"
 
 import "./header.sass"
+
 
 export function Header() {
     const token = cookies.get("access_token")
 
     return(
+        <>
         <header className="header">
             <div className="wrapper wrapper-header">
-                <div className="header__logo-container">
-                    <Link className="header__logo-link" to="/">
-                        <img className="header__logo" src="/static/logo.svg" alt="logo" />
-                    </Link>
-                </div>
-                <nav className="header__nav">
-                    <ul className="header__nav-container">
-                        <li className="header__nav-item">
-                            <Link className="header__item-link hover" to="/create">
-                                Создать
-                            </Link>
+                <Link className="logo" to="/">
+                    Wallpaper
+                </Link>
+                <div className="header__container">
+                    <MainSearch/>
+                    <ul className="header-menu"> 
+                        <li className="header-menu__item">
+                            <img className="header-menu__img" src="/static/notifications.svg" alt="notifications" />  
                         </li>
-                        <li className="header__nav-item">
-                            <Link className="header__item-link hover" to="/popular">
-                                Популярные
-                            </Link>
+                        <li className="header-menu__item">
+                            <img className="header-menu__img" src="/static/chat.svg" alt="chat" />
                         </li>
                     </ul>
-                </nav>
-                <div className="header__auth-container">
-                    <Link className="header__auth-link" to="/auth/login">
-                        <img className={token === undefined ? "header__auth-img" : "header__auth-img header__auth-img_big" }
-                            src={token === undefined ? "/static/login.svg" : "/static/user.svg" } alt="auth" />
-                    </Link>
+                    <span className="line"></span>
+                    <div className="header__auth-container">
+                        {token === undefined
+                            ? <Link className="header__auth-link" to="/auth/login">
+                                <img className="header__auth-img" src="/static/user.svg" alt="auth" />
+                            </Link> 
+                            : "Вывести аву"
+                        }
+                    </div>
                 </div>
             </div>
         </header>
+        <SubMenu />
+        </>
     )
 }
