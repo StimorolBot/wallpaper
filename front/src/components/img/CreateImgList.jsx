5e31e324b2567885f0f146f3289ab6bac18837d2
bot <maximbotpy@gmail.com>
2024-11-29 22:35:34 +0300

    refactor: перенес меню с реакциями в отдельный компонент

--- front/src/components/img/CreateImgList.jsx
+++ front/src/components/img/CreateImgList.jsx
@@ -1,28 +1,12 @@
 import { Link } from "react-router-dom"
 
-import { api } from "../../api/config"
-import { useFetch } from "../hook/useFetch"
 import { BtnDownload } from "../ui/btn/BtnDownload"
+import { ReactionMenu } from "../ui/menu/ReactionMenu"
 
 import "./style/create_img_list.sass"
 
 
 export function CreateImgList({imgList, setImgList, lastElementRef, flag=true}){
-
-    const [setReaction, isLoading, error] = useFetch(
-        async (event, reaction, item, index) => {
-            event.preventDefault()
-
-            if (imgList[index].reaction == reaction){
-                imgList[index].reaction = null  
-            }
-            else{
-                imgList[index].reaction = reaction
-            }
-            setImgList([...imgList])         
-            await api.post("/set-reaction", {"reaction": reaction, "img_uuid": item.uuid_img})
-        }
-    )
         
     return(
         <>
@@ -42,21 +26,10 @@ export function CreateImgList({imgList, setImgList, lastElementRef, flag=true}){
                             src={"data:image/jpeg;base64," + item["img_base64"]}
                         />
                     </Link>
-                    <div className="reaction-img__hov">
-                        <img 
-                            className={item.reaction == true ? "like like_active": "like"}
-                            src="/static/like.svg" alt="like"
-                            onClick={(event) => setReaction(event, true, item, index)}
-                        />
-                        <img
-                            className={item.reaction == false ? "dislike dislike_active": "dislike"}
-                            src="/static/dislike.svg" alt="dislike"
-                            onClick={(event) => setReaction(event, false, item, index)}
-                        />
-                    </div>
+                    <ReactionMenu item={item} index={index} imgList={imgList} setImgList={setImgList}/>
                 </div>
             )})}
-        { flag && <span ref={lastElementRef}></span> }
+        { flag && <span className="last-element" ref={lastElementRef}></span> }
         </>
     )
 }
