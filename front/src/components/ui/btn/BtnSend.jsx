aad3204a7b73b7171cde3748d60b2a5817fb1e21
bot <maximbotpy@gmail.com>
2024-12-01 23:59:37 +0300

    feat: добавил hover

--- front/src/components/ui/btn/BtnSend.jsx
+++ front/src/components/ui/btn/BtnSend.jsx
@@ -3,7 +3,7 @@ import "./style/btn-send.sass"
 
 export function BtnSend({ children, ...props }) {
     return(
-        <button className="btn-send" {...props}>
+        <button className="btn-send hover" {...props}>
             { children }
         </button> 
     )
