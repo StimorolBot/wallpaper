9b8fe74ce83fbc219cc2d021a8b3ffc447c3e3b2
bot <maximbotpy@gmail.com>
2024-12-02 21:44:06 +0300

    feat: добавил валидацию uuid

--- back/src/app/img/schemas.py
+++ back/src/app/img/schemas.py
@@ -2,13 +2,12 @@ from typing_extensions import Annotated
 from fastapi import status, HTTPException
 from pydantic import BaseModel, WrapValidator
 
-from core.my_functools import valid_len
 from src.app.img.enums.style_img import StyleImg
-from core.my_functools import valid_forbidden_symbols
+from core.my_functools import valid_len, valid_uuid
 
 
 def valid_size_img(size: int, handler) -> int:
-    if size in [1024, 576, 680]:
+    if 249 < int(size) or int(size) > 1024:
         return size
 
     raise HTTPException(
@@ -19,12 +18,12 @@ def valid_size_img(size: int, handler) -> int:
 
 def valid_prompt_img(prompt: str, handler) -> str:
     valid_len(val=prompt, min_val=4, max_val=1000)
-    valid_forbidden_symbols(prompt)
     return prompt
 
 
 ValidSizeImg = Annotated[int, WrapValidator(valid_size_img)]
 ValidPromptImg = Annotated[str, WrapValidator(valid_prompt_img)]
+ValidUuidImg = Annotated[str, WrapValidator(valid_uuid)]
 
 
 class ImageSchemas(BaseModel):
@@ -32,9 +31,13 @@ class ImageSchemas(BaseModel):
     style: StyleImg
     width: ValidSizeImg = 1024
     height: ValidSizeImg = 1024
+    is_public: bool = False
 
 
 class AllImageDTO(BaseModel):
     img_base64: str
-    uuid_img: str
+    uuid_img: ValidUuidImg
     reaction: bool | None = None
+
+class PublishSchemas(BaseModel):
+    uuid_img: ValidUuidImg
