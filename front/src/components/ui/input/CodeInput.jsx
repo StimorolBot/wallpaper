520a74e04668397bc92edf4393cbb82e50f167cd
bot <maximbotpy@gmail.com>
2024-12-02 21:35:48 +0300

    feat: добавил валидацию

--- front/src/components/ui/input/CodeInput.jsx
+++ front/src/components/ui/input/CodeInput.jsx
@@ -1,37 +1,39 @@
-import { AuthInput } from "./AuthInput"
+import { api } from "../../../api/config"
+import { AuthInput } from "./auth/AuthInput"
 import { useFetch } from "../../hook/useFetch"
+import { useState } from "react"
+
+import "./style/code_input.sass"
 
 
-export function CodeInput({authData, setAuthData}){
+export function CodeInput({register, errors, emailType, handleSubmit}){
+    const [isSendCode, setIsSendCode] = useState(false)
     
-    const [getCode, isLoadingCode, errorCode] = useFetch(
-        async (event) => {
+    const [request, isLoading, error] = useFetch(
+        async (event, data) => {
             event.preventDefault()
-            if (authData["email"] !== null){
-                await api.post("/auth/get-code",
-                    {"email_type": "CONFIRM", "email": authData["email"]},
-                    {headers:{}}
-                )
-                .then((response) => {
-                    console.log(response.data)
-                })
-                .catch((error) => {
-                    console.log(error)
-                })
-            }
+            setIsSendCode(true)
+            setTimeout(() => { setIsSendCode(false) }, 40000)
+            await api.post("/auth/get-code", {"email_type": emailType, "email": data})
         }
     )
 
+    const validForm = async (data, event) => {
+        if (data.email)
+            await request(event, data.email)
+    }
+
     return(
-        <AuthInput
-            id={"code-confirm-input"}
-            lblTitle={"Код подтверждения"}
-            type={"text"}
-            minLength={6}
-            maxLength={6}
-            inputType={"code"}
-            getCode={getCode}
-            onChange={(event) => setAuthData({...authData, "code_confirm": event.target.value})}
-        />
+        <div className="code-input">
+            <AuthInput id={"code"} lblTitle={"Код подтверждения"} errorMsg={errors?.code_confirm?.message}
+                maxLength={6} isSendCode={isSendCode} onClick={handleSubmit(validForm)}
+                register={register("code_confirm", {
+                    minLength: {value: 6, message: "Длинна поля должна быть 6 символов"},
+                    maxLength: {value: 6, message: "Длинна поля должна быть 6 символов"},
+                    pattern: {value: /^[a-zA-Z0-9]*$/, message: "Некорректный код подтверждения"}
+                }
+            )}/>
+            
+        </div>        
     )
 }
