6d71744a1ea2e2c2365cc54b2ee3de170658e54a
bot <maximbotpy@gmail.com>
2024-12-04 22:54:40 +0300

    init

---
+++ back/src/app/auth/__init__.py
@@ -0,0 +1,6 @@
+from src.app.auth.model import AuthTable
+from src.app.img.model import ImgTable
+from src.app.user.model import UserTable
+from src.app.img.reaction.model import ReactionTable
+
+__all__ = ["AuthTable", "ImgTable", "UserTable", "ReactionTable"]
---
+++ back/src/app/rmq/config.py
@@ -0,0 +1,17 @@
+from aio_pika import connect
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    HOST: str = "localhost"
+    PORT: int = 5672
+    LOGIN: str = "guest"
+    PASSWORD: str = "guest"
+    ROUTING_KEY: str = "img"
+
+settings = Settings()
+
+
+async def get_connection() -> connect:
+    return await connect(host=settings.HOST, port=settings.PORT, login=settings.LOGIN, password=settings.PASSWORD)
+
+
