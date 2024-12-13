17cddf7ac02846560187616f55c6c86a487f2f36
bot <maximbotpy@gmail.com>
2024-12-13 20:40:15 +0300

    refactor: добавил LoaderV2

--- front/src/components/ui/btn/BtnPublish.jsx
+++ front/src/components/ui/btn/BtnPublish.jsx
@@ -1,19 +1,26 @@
+import { useRef } from "react"
 import { api } from "../../../api/config"
 import { useFetch } from "../../hook/useFetch"
+import { LoaderV2 } from "../../loader/LoaderV2"
 
 import "./style/btn_publish.sass"
 
 
 export function BtnPublish({uuidImg}){
+    const isPublic = useRef(false)
+    
     const [request, isLoading, error] = useFetch(
         async (event) => {
             event.preventDefault()
-            await api.post("/publish", {uuid_img: uuidImg})
+            await api.post("/publish", {uuid_img: uuidImg}).then(response => isPublic.current = true)
         }
     )
     return(
         <button className="btn-publish hover" onClick={async (event) => await request(event)}> 
-            Опубликовать
+            { isLoading
+                ? <LoaderV2/>
+                : isPublic.current ? "Изображение опубликовано" : "Опубликовать"
+            }            
         </button>
     )
 }
