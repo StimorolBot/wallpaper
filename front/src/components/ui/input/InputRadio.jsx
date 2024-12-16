9ac7608a3c06315692dd9f7f9c073cff4fb5667d
bot <maximbotpy@gmail.com>
2024-12-17 00:28:27 +0300

    refactor: добавил класс для дива, внес незначительные изменения

--- front/src/components/ui/input/InputRadio.jsx
+++ front/src/components/ui/input/InputRadio.jsx
@@ -1,10 +1,10 @@
 import "./style/input_radio.sass"
 
-export function InputRadio({id, name, text, ...props}){
+export function InputRadio({id, name, children, ...props}){
     return(
-        <div>
+        <div className="radio-input__container">
             <input className="radio-input" id={id} type="radio" name={name} {...props}/>
-            <label className="radio-lbl" htmlFor={id}>{text}</label>
+            <label className="radio-lbl" htmlFor={id}>{children}</label>
         </div>
     )
 }
