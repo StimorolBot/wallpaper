a1febba5e4c0ea91a6a18bb5ffdab4de20954eaa
bot <maximbotpy@gmail.com>
2024-12-17 02:01:25 +0300

    refactor: перенес все функции с валидацией в core/validator

--- back/core/my_functools.py
+++ back/core/my_functools.py
@@ -1,58 +1,13 @@
-import json
 import string
 import secrets
-from uuid import uuid4, UUID
-from pydantic import EmailStr
+from uuid import uuid4
 from fastapi import status, HTTPException, Request
 
-from src.config import redis
 from core.logger import auth_logger
 
 
 def generate_uuid() -> str:
     return uuid4().hex
-
-
-def valid_isalnum(val: str):
-    if val.isalnum() is False:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Поле может содержать только буквы и цифры")
-
-
-def valid_password(val: str):
-    symbols = {
-        "[", "]", "\\", "$", "|", "?", "*", "+",
-        "(", ")", "{", "}", "/", "#", "'", '"',
-        "@", " ", "!", "~", "`", "%", "=", "&"
-    }
-    if symbols & set(val):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Пароль но должен содержать: {symbols}")
-
-
-def valid_len(val: str, min_val: int, max_val: int):
-    if len(val) < min_val or len(val) > max_val:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Поле должно быть в пределах от {min_val} до {max_val} символов"
-        )
-
-
-def valid_uuid(uuid: str, handler, version: int = 4) -> str:
-    try:
-        return UUID(uuid, version=version).hex
-    except ValueError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Некорректный uuid")
-
-
-async def set_redis(name: EmailStr, value: dict, ex: int = 2):
-    value_str = json.dumps(value)
-    await redis.set(name=name, value=value_str, ex=ex)
-
-
-async def get_redis(key: EmailStr) -> dict | None:
-    data_dict = await redis.get(key)
-    if not data_dict:
-        return None
-    return json.loads(data_dict)
 
 
 def generate_code(code_len: int = 6) -> str:
