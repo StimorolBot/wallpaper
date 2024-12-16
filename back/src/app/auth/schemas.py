6ef1eb05d4cc7649f1f4a5fc00f5d09bf5b4eca3
bot <maximbotpy@gmail.com>
2024-12-17 00:35:09 +0300

    refactor: перенес функции с валидацией в /core/validator.py

--- back/src/app/auth/schemas.py
+++ back/src/app/auth/schemas.py
@@ -1,32 +1,7 @@
-from typing_extensions import Annotated
-from pydantic import BaseModel, ConfigDict, WrapValidator, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 from celery_task.smtp.type_email import TypeEmail
-from core.my_functools import valid_password as valid_password_symbol
-from core.my_functools import valid_isalnum, valid_len
-
-
-def valid_name(name: str, handler) -> str:
-    valid_len(name, 4, 20)
-    valid_isalnum(name)
-    return name
-
-
-def valid_password(password: str, handler) -> str:
-    valid_len(password, 8, 32)
-    valid_password_symbol(password)
-    return password
-
-
-def valid_code_confirm(code_confirm: str, handler) -> str:
-    valid_len(code_confirm, min_val=5, max_val=7)
-    valid_isalnum(code_confirm)
-    return code_confirm
-
-
-ValidName = Annotated[str, WrapValidator(valid_name)]
-ValidPassword = Annotated[str, WrapValidator(valid_password)]
-ValidCodeConfirm = Annotated[str, WrapValidator(valid_code_confirm)]
+from core.validator import ValidName, ValidPassword, ValidCodeConfirm
 
 
 class Email(BaseModel):
