eb7a9f32382ea9300385d1548a501b6f1a24133a
bot <maximbotpy@gmail.com>
2024-12-17 21:52:21 +0300

    feat: добавил валидацию uuid и operation_type

--- back/src/app/img/reaction/schemas.py
+++ back/src/app/img/reaction/schemas.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
 
+from core.validator import ValidUuid
+from src.app.img.enums.operation_type import Operation
+
 
 class ReactionSchemas(BaseModel):
     reaction: bool
-    img_uuid: str
+    img_uuid: ValidUuid
+    operation_type: Operation
