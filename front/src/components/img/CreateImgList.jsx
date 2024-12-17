3dab79cd224e815235563f4bc50e68ecd3e775ed
bot <maximbotpy@gmail.com>
2024-12-17 21:49:57 +0300

    refactor: заменил is_like -> reaction, убрал лишние пропсы

--- front/src/components/img/CreateImgList.jsx
+++ front/src/components/img/CreateImgList.jsx
@@ -7,12 +7,23 @@ import "./style/create_img_list.sass"
 
 
 export function CreateImgList({imgList, setImgList, lastElementRef, flag=true}){
-        
+    const convertReaction = (item) => {
+        if (item.is_like === 1){
+            item.reaction = true
+            delete item.is_like
+        }
+        else if (item.is_dislike === 1){
+            item.reaction = false
+            delete item.is_dislike
+        }            
+    }
+
     return(
         <>
         { imgList?.map((item, index) => {
             return(
                 <div className="img__wrapper" key={index}>
+                    {convertReaction(item)}
                     <BtnDownload
                         href={`data:image/jpeg;base64,${item["img_base64"]}`}
                         name={item["uuid_img"]}
@@ -26,7 +37,7 @@ export function CreateImgList({imgList, setImgList, lastElementRef, flag=true}){
                             src={"data:image/jpeg;base64," + item["img_base64"]}
                         />
                     </Link>
-                    <ReactionMenu item={item} index={index} imgList={imgList} setImgList={setImgList}/>
+                    <ReactionMenu item={item}/> 
                 </div>
             )})}
         { flag && <span className="last-element" ref={lastElementRef}></span> }
