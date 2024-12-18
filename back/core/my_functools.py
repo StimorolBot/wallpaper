fc4fb98b76583198d002f86090cefcd77f5edc01
bot <maximbotpy@gmail.com>
2024-12-18 15:06:06 +0300

    feat: добавил функцию для получения даты в utc

--- back/core/my_functools.py
+++ back/core/my_functools.py
@@ -1,6 +1,7 @@
 import string
 import secrets
 from uuid import uuid4
+from datetime import datetime, UTC
 from fastapi import status, HTTPException, Request
 
 from core.logger import auth_logger
@@ -13,6 +14,11 @@ def generate_uuid() -> str:
 def generate_code(code_len: int = 6) -> str:
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(secrets.choice(letters_and_digits) for _ in range(code_len))
+
+
+def get_unc_now(f: str = "%d-%m-%Y, %H:%M:%S") -> datetime:
+    date_str = datetime.now(UTC).strftime(f)
+    return datetime.strptime(date_str, f)
 
 
 def get_info_from_headers(request: Request) -> list:
