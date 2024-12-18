9c25f852f66976b6c6a4330e0105f6696c41bbc7
bot <maximbotpy@gmail.com>
2024-12-18 19:44:44 +0300

    refactor: внес незначительные изменения

--- front/src/page/Home.jsx
+++ front/src/page/Home.jsx
@@ -1,6 +1,6 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
-import { useState, useRef } from "react"
 import { Header } from "../components/header/Header"
 import { Footer } from "../components/footer/Footer"
 import { Pagination } from "../components/pagination/Pagination"
