05f80da71601c79c3eb71582fc7066379f716b6c
bot <maximbotpy@gmail.com>
2024-12-18 15:08:15 +0300

    refactor: изменил current_time -> utc

--- back/src/app/auth/model.py
+++ back/src/app/auth/model.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 from typing import TYPE_CHECKING, List
-
-from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.model import Base
-from core.my_functools import generate_uuid
+from core.my_functools import generate_uuid, get_unc_now
 
 if TYPE_CHECKING:
     from src.app.img.model import ImgTable
@@ -18,8 +16,8 @@ class AuthTable(Base):
     uuid_user: Mapped[str] = mapped_column(primary_key=True, default=generate_uuid)
     user_name: Mapped[str] = mapped_column()
     email: Mapped[str] = mapped_column(unique=True, index=True)
+    date_register: Mapped[datetime] = mapped_column(default=get_unc_now)
     hash_password: Mapped[str] = mapped_column()
-    date_register: Mapped[datetime] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
     is_active: Mapped[bool] = mapped_column(default=True)
     is_superuser: Mapped[bool] = mapped_column(default=False)
     is_verified: Mapped[bool] = mapped_column(default=False)
