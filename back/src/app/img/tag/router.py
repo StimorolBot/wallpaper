d323bcc091da3125a33209a59ae49f7b44919195
bot <maximbotpy@gmail.com>
2024-12-22 00:47:57 +0300

    feat: добавил роут для получения популярных тегов refactor: исправил create_tag

--- back/src/app/img/tag/router.py
+++ back/src/app/img/tag/router.py
@@ -1,16 +1,23 @@
-from fastapi import APIRouter, Depends, status
+from sqlalchemy import select, func, desc
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi_cache.decorator import cache
+from fastapi_pagination import Page, paginate
+from fastapi import APIRouter, Depends, status, HTTPException
 
 from core.crud import crud
 from core.logger import img_logger
 from src.db.get_session import get_async_session
 
 from src.app.img.model import ImgTable
-from src.app.img.tag.schemas import TagSchemas
+from src.app.img.tag.schemas import TagSchemas, PopularTagDTO
+from src.app.img.tag.model import TagBufferTable, TagTable
+
+from src.app.img.tag.stmt import add_tag
 from src.app.img.get_user import get_user_by_token
 
-from src.app.llama.operation_type import Operation
-from src.app.llama.llama_func import create_tag as llama_create_tag
+from src.app.llama.ollama_helper import Operation
+from src.app.llama.llama_func import ollama_helper
 
 tag_router = APIRouter(prefix="/tag", tags=["tag"])
 
@@ -22,17 +29,43 @@ async def create_tag(
         session: AsyncSession = Depends(get_async_session)
 ):
     img_info = await crud.read(session=session, table=ImgTable, uuid_user=user["sub"], uuid_img=tag_data.uuid_img)
-
-    if tag_data.is_automatically:
-        tag_data.tag_list = await llama_create_tag(img=img_info.img_base64, operation=Operation.CREATE_TAG)
-
-    await crud.update(
-        session=session, table=ImgTable,
-        data={"img_tag": tag_data.tag_list},
-        uuid_user=user["sub"], uuid_img=tag_data.uuid_img
+    try:
+        if tag_data.is_automatically:
+            tag_data.tag_list = await ollama_helper(img=img_info.img_base64, operation=Operation.CREATE_TAG)
+    except AttributeError:
+        img_logger.warning("Не удалось найти изображение с заданным uuid: %s", tag_data.uuid_img)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Не удалось найти изображение с заданным uuid: {tag_data.uuid_img}"
+        )
+    await add_tag(session=session, tag_list=tag_data.tag_list, uuid_img=tag_data.uuid_img)
+    img_logger.debug(
+        "Пользователь %s добавил теги %s к изображению %s",
+        user["sub"], tag_data.tag_list, tag_data.uuid_img
     )
-    img_logger.debug("Пользователь %s добавил теги к изображению %s", user["sub"], tag_data.uuid_img)
     return tag_data.tag_list
+
+
+@tag_router.get("/get-popular-tag")
+# @cache(expire=120)
+async def get_popular_tag(session: AsyncSession = Depends(get_async_session)) -> Page:
+    tag_count = func.count(TagBufferTable.tag_id).label("tag_count")
+    label = TagTable.tag.label("label")
+    value = TagBufferTable.tag_id.label("value")
+
+    query = (
+        select(tag_count, label, value)
+        .select_from(TagBufferTable)
+        .join(TagTable, TagTable.id == TagBufferTable.tag_id)
+        .group_by(TagTable.id, value)
+        .order_by(desc(tag_count))
+    )
+
+    res = await session.execute(query)
+    popular_tag_list = res.mappings().all()
+
+    data = [PopularTagDTO.model_validate(tag, from_attributes=True) for tag in popular_tag_list]
+    return paginate(data)
 
 
 @tag_router.patch("/update")
