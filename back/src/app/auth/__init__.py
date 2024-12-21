ee9e4121248a1583edfd3a897021d351d630d21d
bot <maximbotpy@gmail.com>
2024-12-21 22:31:00 +0300

    feat: добавил TagTable

--- back/src/app/auth/__init__.py
+++ back/src/app/auth/__init__.py
@@ -2,5 +2,6 @@ from src.app.auth.model import AuthTable
 from src.app.img.model import ImgTable
 from src.app.user.model import UserTable
 from src.app.img.reaction.model import ReactionTable
+from src.app.img.tag.model import TagTable
 
-__all__ = ["AuthTable", "ImgTable", "UserTable", "ReactionTable"]
+__all__ = ["AuthTable", "ImgTable", "UserTable", "ReactionTable", "TagTable"]
