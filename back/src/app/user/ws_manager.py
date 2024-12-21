7fd84518d528aa714ccf351310495e40558ef706
bot <maximbotpy@gmail.com>
2024-12-22 00:54:46 +0300

    refactor: заменил datetime.now(UTC) -> get_unc_now()

--- back/src/app/user/ws_manager.py
+++ back/src/app/user/ws_manager.py
@@ -1,8 +1,9 @@
-from datetime import datetime, UTC
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.crud import crud
 from core.logger import user_logger
+from core.my_functools import get_unc_now
+
 from src.app.user.model import UserTable
 from src.redis.redis_func import set_redis, del_redis
 
@@ -18,11 +19,11 @@ class ConnectionManager:
             user_logger.debug("Пользователь %s зашел на сайт", uuid_user)
 
     async def disconnect(self, uuid_user: str, session: AsyncSession):
-        last_visit_time = datetime.now(UTC)
+        last_visit_time = get_unc_now()
+        self.online_user.remove(uuid_user)
         await crud.update(session=session, table=UserTable, data={"last_visit": last_visit_time}, uuid_user=uuid_user)
         await del_redis(uuid_user)
-        self.online_user.remove(uuid_user)
-        user_logger.debug("Пользователь %s покинул сайт в %s", uuid_user, last_visit_time)
+        user_logger.debug("Пользователь %s покинул сайт", uuid_user)
 
 
 ws_manager = ConnectionManager()
