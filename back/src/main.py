b6c36db88ee9058743f2ec6c1b574679b193673d
bot <maximbotpy@gmail.com>
2024-12-24 21:06:43 +0300

    feat: подключил tag_router

--- back/src/main.py
+++ back/src/main.py
@@ -9,11 +9,12 @@ from fastapi_pagination import add_pagination
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 
-from src.config import redis
+from src.redis.config import redis
 from src.app.img.router import img_router
 from src.app.auth.router import register_router
 from src.app.auth.token.router import jwt_router
 from src.app.user.router import user_router
+from src.app.img.tag.router import tag_router
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
@@ -27,6 +28,9 @@ app.include_router(img_router)
 app.include_router(register_router)
 app.include_router(jwt_router)
 app.include_router(user_router)
+app.include_router(tag_router)
+
+add_pagination(app)
 
 origins = [
     "http://localhost:5173",
@@ -44,8 +48,7 @@ app.add_middleware(
     allow_headers=[
         "Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
         "Access-Control-Allow-Origin", "Authorization"
-    ],
-    expose_headers=["Filter-Time"]
+    ]
 )
 
 
