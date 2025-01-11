5bb93eb1842b11462cd19d30c7740363f8dd4f6d
bot <maximbotpy@gmail.com>
2025-01-11 20:02:29 +0300

    refactor: переименовал Home.jsx -> Main.jsx, убрал empty-list__msg

---
+++ front/src/page/Main.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+import { Header } from "../components/header/Header"
+import { Footer } from "../components/footer/Footer"
+import { Pagination } from "../components/Pagination"
+
+
+export function Main(){
+    const [imgList, setImgList] = useState([])
+
+    return(        
+        <div className="wrapper">
+            <Header/>
+            <main className="main">
+                <h1 className="title-page"> Главная страница </h1>
+                <div className="container">
+                    <Pagination path={"/"} itemList={imgList} setItemList={setImgList}/>
+                </div>
+            </main>
+            <Footer/>
+        </div>
+    )
+}
