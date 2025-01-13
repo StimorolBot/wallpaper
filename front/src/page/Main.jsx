aeaf1116470907aa15d9d01b6409777877ea2fc6
bot <maximbotpy@gmail.com>
2025-01-13 23:09:05 +0300

    refactor: изменил пути импорта

--- front/src/page/Main.jsx
+++ front/src/page/Main.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 import { Header } from "../components/header/Header"
 import { Footer } from "../components/footer/Footer"
-import { Pagination } from "../components/Pagination"
+import { Pagination } from "../components/pagination/Pagination"
 
 
 export function Main(){
