a00ee038e3c65a46a699950152f956378a71d8f0
bot <maximbotpy@gmail.com>
2025-01-13 23:13:52 +0300

    refactor: перенес svg в отдельный файл

--- front/src/components/loader/Loader.jsx
+++ front/src/components/loader/Loader.jsx
@@ -3,7 +3,9 @@ import "./style/loader.sass"
 export function Loader({loaderMsg=null}){
     return(
         <div className="loader">
-            <img className="loader-img" src="/static/loader.svg" alt="loader" />
+            <svg className="loader-img">
+                <use xlinkHref="/static/main.svg#loader"></use>    
+            </svg>
             <p className="loader-text">
                 {loaderMsg}
             </p>
