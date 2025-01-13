4e674f83aa74c3d64c410942b44ccf3f047dc9c2
bot <maximbotpy@gmail.com>
2025-01-13 20:52:43 +0300

    refactor: перенес в ./input, переименовал EmailInput -> InputEmail

--- front/src/components/ui/input/InputEmail.jsx
+++ front/src/components/ui/input/InputEmail.jsx
@@ -1,14 +1,13 @@
-import { AuthInput } from "./AuthInput"
+import { InputBase } from "./InputBase"
 
-
-export function EmailInput({register, errors}){
+export function InputEmail({register, errors}) {
     return(
-        <AuthInput id="email" lblTitle="Почта" errorMsg={errors?.email?.message} maxLength={24}
+        <InputBase id="email" type="email" lblText={"Email"} maxLength={24} minLength={8}
             register={register("email", {
                 minLength: {value: 8, message: "Длинна поля должна быть от 8 символов"},
                 maxLength: {value: 24, message: "Длинна поля должна быть до 24 символов"},
                 pattern: {value: /(^[a-zA-Z0-9_-]+@[mail|gmail|]+\.[ru|com]+)/, message: "Неверный формат почты"}
-            }
-        )}/>
+            })} errorMsg={<p className="input__error-msg">{errors?.email?.message}</p>}
+        />
     )
 }
