7939686e64bdcb8cc3535fc4d3188f7ed7d012dd
bot <maximbotpy@gmail.com>
2025-01-14 19:12:55 +0300

    refactor: переписал html содержимое страницы

--- front/src/page/CreateImg.jsx
+++ front/src/page/CreateImg.jsx
@@ -1,101 +1,110 @@
 import { useState } from "react"
 
 import { api } from "../api/config"
+import { useFetch } from "../components/hook/useFetch"
 
 import { Header } from "../components/header/Header"
 import { Footer } from "../components/footer/Footer"
 import { Loader } from "../components/loader/Loader"
+
 import { BtnSend } from "../components/ui/btn/BtnSend"
-import { useFetch } from "../components/hook/useFetch"
-import { Textarea } from "../components/ui/input/Textarea"
+import { BtnPublish } from "../components/ui/btn/BtnPublish"
+import { Textarea } from "../components/ui/textarea/Textarea"
 import { InputRadio } from "../components/ui/input/InputRadio"
 import { InputRange } from "../components/ui/input/InputRange"
+import { CreateTag } from "../components/img/CreateTag"
 
-import "./style/create_img.sass"
+import style from "./style/create_img.module.sass"
 
 
 export function CreateImg() {
-    const [img, setImg] = useState(null)
-    
+
+    const [response, setResponse] = useState({"uuid_img": null, "base64_img": null}) 
     const [imgParams, setImgParams] = useState({
-        "prompt": "", "style": "DEFAULT", "width": 512, "height": 512
+        "prompt": null, "negative_prompt": null, "style": "DEFAULT", "width": 512, "height": 512
     })
 
     const [request, isLoading, error] = useFetch(
         async (event) => {
             event.preventDefault()
-            await api.post("/create", imgParams).then((response) => {setImg(response.data)})
+            await api.post("/create", imgParams).then((r) => {setResponse({...r.data})})
         }
     )
-    
+
     return(
-        <>
-        <Header/>
-        <section className="img-create-section">
-            <h2 className="hidden">
-                Генерация изображения
-            </h2>
-            <div className="wrapper">
-                <div className="img-create__container">
-                    <div className="img-generate__container">
-                        { isLoading 
-                            ? <Loader loaderMsg={"Пожалуйста подождите, идет генерация изображения..."}/> 
-                            : <div className={img ? "img-container" : "img-container img-container_empty"}>
-                                <img className="img-create"
-                                    src={`data:image/jpeg;base64,${img}`} alt="img-ai"
+        <div className="wrapper">
+            <Header/>
+            <main className="main main_flex">
+                <h1 className="title-page"> Генерация изображения </h1>
+                <div className="container">
+                    <div className={style.create__inner}>
+                    {isLoading
+                    ? <Loader loaderMsg={"Пожалуйста подождите, идет генерация изображения..."}/>
+                    :<>
+                        <div className={style.create__img}>
+                            {response.base64_img && 
+                                <img src={`data:image/jpeg;base64,${response?.base64_img}`} alt="img-ai"/>
+                            }
+                            {response?.tag &&
+                                <ul className={style.tag__list}>
+                                    {response.tag.map((tag, index) => {
+                                        return (
+                                            <li className={style.tag__item} key={index}>{tag}</li>
+                                        )
+                                    })}
+                                </ul>
+                            }
+                        </div>
+                        <form className={style.form__create} onSubmit={async (event) => await request(event)}>
+                            <div className={style.formStyle__container}>
+                                <InputRadio id={"style-default"} name={"style-img"}
+                                    onChange={()=> setImgParams({...imgParams, style:"DEFAULT"})}
+                                >
+                                    Стандартный
+                                </InputRadio>
+                                <InputRadio id={"style-uhd"} name={"style-img"}
+                                    onChange={()=> setImgParams({...imgParams, style:"UHD"})}
+                                >
+                                    Детальное фото
+                                </InputRadio>
+                                <InputRadio id={"style-anime"} name={"style-img"}
+                                    onChange={()=> setImgParams({...imgParams, style:"ANIME"})}
+                                >
+                                    Аниме
+                                </InputRadio>
+                                <InputRadio id={"style-kandinsky"} name={"style-img"}
+                                    onChange={()=> setImgParams({...imgParams, style:"KANDINSKY"})}
+                                >
+                                    Кандинский
+                                </InputRadio>
+                            </div>
+                            <div className={style.formSize__container}>
+                                <InputRange titleLabel={"width"} imgParams={imgParams} setImgParams={setImgParams} />
+                                <InputRange titleLabel={"height"} imgParams={imgParams} setImgParams={setImgParams} />
+                            </div>
+                            {response.base64_img && 
+                                <CreateTag response={response} setResponse={setResponse}/>
+                            }
+                            <div className={style.formPrompt__container}>
+                                <Textarea maxLength="1000" placeholder="Промт" keyDict={"prompt"}
+                                    value={imgParams} setValue={setImgParams} required
+                                />
+                                <Textarea maxLength="1000" placeholder="Негативный промт"
+                                    value={imgParams} setValue={setImgParams} keyDict={"negative_prompt"}
                                 />
                             </div>
-                        }
+                            <div className={style.formBtn__container}>
+                                <BtnSend> Сгенерировать </BtnSend>
+                                {response.base64_img && response?.tag &&
+                                    <BtnPublish uuidImg={response?.uuid_img} tagList={response.tag}/>
+                                }
+                            </div>
+                        </form>
+                    </>}
                     </div>
-                    <form className={
-                            isLoading
-                            ? "form__create-img form__create-img_active"
-                            : "form__create-img"
-                        }
-                        onSubmit={async (event)=> await request(event)}
-                    >
-                        <div className="checkbox-style__container">
-                            <InputRadio id={"style-default"} name={"style-img"} text={"Стандартный"}
-                                onChange={()=> setImgParams({...imgParams, style:"DEFAULT"})}
-                            />
-                                
-                            <InputRadio id={"style-uhd"} name={"style-img"} text={"Детальное фото"}
-                                onChange={()=> setImgParams({...imgParams, style:"UHD"})}
-                            />
-                                
-                            <InputRadio id={"style-anime"} name={"style-img"} text={"Аниме"}
-                                onChange={()=> setImgParams({...imgParams, style:"ANIME"})}
-                            />
-                                
-                            <InputRadio id={"style-kandinsky"} name={"style-img"} text={"Кандинский"}
-                                onChange={()=> setImgParams({...imgParams, style:"KANDINSKY"})}
-                            />
-                        </div>
-                        <div className="size__container">
-                            <InputRange
-                                titleLabel={"width"}  
-                                imgParams={imgParams}
-                                setImgParams={setImgParams}
-                            />
-                            <InputRange 
-                                titleLabel={"height"} 
-                                imgParams={imgParams}
-                                setImgParams={setImgParams}
-                            />
-                        </div>    
-                        <div className="prompt__container">
-                            <Textarea maxLength="1000" placeholder="Введите промт для генерации изображения"
-                                value={imgParams} setValue={setImgParams} 
-                            />
-                        </div>
-                        <div className="form__create-btn-container hover">
-                            <BtnSend> Сгенерировать </BtnSend> 
-                        </div>
-                    </form>
-                </div>    
-            </div>
-        </section>
-        <Footer/>
-        </>
+                </div>
+            </main> 
+            <Footer/>      
+        </div>
     )
 }
