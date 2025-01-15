f47c9e07d47e9435492d2ff9642d55ce4c384fe9
bot <maximbotpy@gmail.com>
2025-01-15 17:44:40 +0300

    refactor: внес незначительные изменения

--- front/src/components/img/CreateItemImg.jsx
+++ front/src/components/img/CreateItemImg.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 import { BtnDownload } from "../ui/btn/BtnDownload"
-import { ReactionMenu } from "../ui/menu/ReactionMenu"
+import { Reaction } from "../reaction/Reaction"
 
 import "./style/create_item_img.sass"
 
@@ -20,22 +20,24 @@ export function CreateItemImg({item, index}){
     }
 
     return(
-        <div className="img__wrapper" key={index}>
-            {convertReaction(item)}
+        <li className="create-img__item" key={index}>
+            {
+                convertReaction(item)
+            }
             <BtnDownload
                 href={`data:image/jpeg;base64,${item["img_base64"]}`}
                 name={item["uuid_img"]}
             />
-            <Link className="img__link" to={`/wallpaper/${item["uuid_img"]}`}>
+            <Link className="create-img__link" to={`/wallpaper/${item["uuid_img"]}`}>
                 <img 
-                    className="img__item"
+                    className="create-img__img" 
                     alt="img-ai"
                     name={item["uuid_img"]}
                     loading="lazy"
                     src={"data:image/jpeg;base64," + item["img_base64"]}
                 />
             </Link>
-            <ReactionMenu item={item}/> 
-        </div>
+            <Reaction item={item}/>
+        </li>
     )
 }
