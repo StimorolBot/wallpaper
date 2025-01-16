d71ce363884200640ab234bce344b0da4092bba6
bot <maximbotpy@gmail.com>
2025-01-16 19:33:35 +0300

    feat: добавил FriendTable

--- back/src/app/__init__.py
+++ back/src/app/__init__.py
@@ -1,7 +1,7 @@
 from src.app.auth.model import AuthTable
 from src.app.img.model import ImgTable
-from src.app.user.model import UserTable
+from src.app.user.model import UserTable, FriendTable
 from src.app.img.reaction.model import ReactionTable
 from src.app.img.tag.model import TagTable
 
-__all__ = ["AuthTable", "ImgTable", "UserTable", "ReactionTable", "TagTable"]
+__all__ = ["AuthTable", "ImgTable", "UserTable", "ReactionTable", "TagTable", "FriendTable"]
