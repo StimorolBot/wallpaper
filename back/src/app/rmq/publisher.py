bce91dadeb12cced789aeb0cb0c62cd899446b22
bot <maximbotpy@gmail.com>
2025-01-27 23:48:13 +0300

    feat: добавил запись в redis

--- back/src/app/rmq/publisher.py
+++ back/src/app/rmq/publisher.py
@@ -1,14 +1,18 @@
+from json import loads
 from aio_pika import Message
 from aio_pika.channel import Channel
 
 from core.logger import rmq_logger
 from src.app.rmq.config import settings
 from src.app.rmq.config import get_connection
+from src.app.redis.redis_func import set_redis
 
 
 async def produce_msg(channel: Channel, msg: bytes):
     queue = await channel.declare_queue(settings.ROUTING_KEY)
     await channel.default_exchange.publish(Message(msg), routing_key=queue.name)
+    msg_data = loads(msg.decode())
+    await set_redis(name=msg_data["subscriber_uuid"], value=msg_data, ex=None)
     rmq_logger.info("Сообщение: '%s' отправлено в очередь: %s", msg.decode(), queue)
 
 
