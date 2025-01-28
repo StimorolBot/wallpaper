3f117dc8c00463924d844711f7e4928b0974d13e
bot <maximbotpy@gmail.com>
2025-01-28 22:12:00 +0300

    feat: добавил роуты для добавления в друзья и получения уведомлений

--- back/src/app/user/router.py
+++ back/src/app/user/router.py
@@ -1,3 +1,4 @@
+import json
 from typing import Annotated
 from jwt.exceptions import DecodeError
 
@@ -5,24 +6,29 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi_cache.decorator import cache
-from fastapi import APIRouter, UploadFile, File, Depends, Cookie, WebSocket
 from starlette.websockets import WebSocketState
+from fastapi import APIRouter, UploadFile, File, Depends, Cookie, WebSocket, status
 
 from core.crud import crud
-from src.app.auth.model import AuthTable
-from src.app.user.model import UserTable
-from src.app.user.schemas import UuidUser
-from src.redis.redis_func import get_redis
-from src.app.user.ws_manager import ws_manager
+from src.app.rmq.publisher import publisher
 from src.db.get_session import get_async_session
 from src.app.auth.token.jwt_token import jwt_token
 from src.app.img.get_user import get_user_by_token
 
+from src.app.auth.model import AuthTable
+from src.app.user.model import UserTable, FriendTable
+
+from src.app.redis.operation_type import Operation
+from src.app.redis.redis_func import get_redis, del_redis
+from src.app.user.ws_manager import ws_manager
+
+from src.app.user.schemas import ActionRequest, AddUserSchema
+
 user_router = APIRouter(tags=["user"], prefix="/user")
 
 
-@user_router.get("")
-# @cache(expire=120) # сделать так, что бы если есть какойто флаг, сбрасывал кеш
+@user_router.get("/info")
+@cache(expire=120)
 async def get_user_info(
         access_token: Annotated[str | None, Cookie()] = None,
         session: AsyncSession = Depends(get_async_session)
@@ -41,54 +47,60 @@ async def get_user_info(
         return None
 
 
-@user_router.websocket("/last-visit")
-async def last_visit(
+@user_router.websocket("/ws")
+async def ws(
         websocket: WebSocket,
         user: dict = Depends(get_user_by_token),
         session: AsyncSession = Depends(get_async_session)
 ):
     await websocket.accept()
-    await ws_manager.connect(user["sub"])
+    await ws_manager.connect(uuid_user=user["sub"], websocket=websocket)
 
     await websocket.receive()
     if websocket.client_state.value == WebSocketState.DISCONNECTED.value:
         await ws_manager.disconnect(user["sub"], session)
 
 
-@user_router.post("/last-visit")
-async def get_last_visit(uuid_user: UuidUser, session: AsyncSession = Depends(get_async_session)) -> dict:
-    user_last_visit = await get_redis(uuid_user.uuid_user)
-
+@user_router.get("/last-visit")
+async def get_last_visit(uuid_user: str, session: AsyncSession = Depends(get_async_session)) -> dict:
+    user_last_visit = ws_manager.online_user.get("uuid_user")
     if user_last_visit:
-        return {"last_visit": "Online"}
+        return {"status": "Online"}
 
-    user_last_visit = await crud.read(session=session, table=UserTable.last_visit, uuid_user=uuid_user.uuid_user)
-    return {"last_visit": f"{user_last_visit}"}
-
-
-@user_router.post("/set-avatar")
-async def set_avatar(img: UploadFile = File(...)):
-    print(img)
+    user_last_visit = await crud.read(session=session, table=UserTable.last_visit, uuid_user=uuid_user)
+    return {"status": user_last_visit}
 
 
 @user_router.post("/subscribe")
-async def subscribe_user(
-        uuid: UuidUser,
+async def subscribe_user(friend_request: ActionRequest, subscriber: dict = Depends(get_user_by_token)) -> dict:
+    data = {
+        "uuid_user": friend_request.uuid_user, "user_name": friend_request.user_name,
+        "subscriber_uuid": subscriber["sub"], "operation": friend_request.operation.value
+    }
+    data_str = json.dumps(data)
+    await publisher(msg=data_str.encode("utf-8"))
+    return {"status_code": status.HTTP_200_OK, "data": "Заявка успешно отправлена"}
+
+
+@user_router.get("/get-notification")
+async def get_notification(user: dict = Depends(get_user_by_token)):
+    data = await get_redis(user["sub"])
+    if data:
+        return ActionRequest.model_validate(data, from_attributes=True)
+
+
+@user_router.patch("/add-friend")
+async def add_friend(
+        user_data: AddUserSchema,
         user: dict = Depends(get_user_by_token),
         session: AsyncSession = Depends(get_async_session)
 ):
-    return
-
-
-@user_router.delete("/unsubscribe")
-async def unsubscribe_user(
-        uuid: UuidUser,
-        user: dict = Depends(get_user_by_token),
-        session: AsyncSession = Depends(get_async_session)
-):
-    return
-
-
-@user_router.delete("/delete-user")
-async def delete_user(user: dict = Depends(get_user_by_token), session: AsyncSession = Depends(get_async_session)):
-    return
+    await del_redis(name=f"{user["sub"]}/{Operation.FRIEND_REQUEST.value}")
+    if user_data.is_add:
+        await crud.update(
+            table=FriendTable,
+            session=session,
+            data={"is_friend": True},
+            uuid_user=user["sub"],
+            subscriber_uuid=user_data.subscriber_uuid
+        )
