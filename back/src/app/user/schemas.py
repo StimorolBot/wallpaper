617891e2d85d745f4365e4975d365427a8ce4c4a
bot <maximbotpy@gmail.com>
2025-01-30 23:00:41 +0300

    feat: добавил схему для валидации изменения аватара

--- back/src/app/user/schemas.py
+++ back/src/app/user/schemas.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel, ConfigDict
+from fastapi import status, HTTPException
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 from core.validator import ValidUuid, ValidName
 from src.app.redis.operation_type import Operation
 
 
-class ActionRequest(BaseModel):
+class ActionRequestSchema(BaseModel):
     uuid_user: ValidUuid
     user_name: ValidName
     operation: Operation
@@ -16,3 +17,14 @@ class ActionRequest(BaseModel):
 class AddUserSchema(BaseModel):
     is_add: bool
     subscriber_uuid: ValidUuid
+
+
+class ChangeAvatarSchema(BaseModel):
+    img_size: int = Field(le=2500000)
+    content_type: str
+
+    @field_validator("content_type")
+    @classmethod
+    def check_content_type(cls, value: str):
+        if value.split("/")[0] != "image":
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Неверный формат файла")
