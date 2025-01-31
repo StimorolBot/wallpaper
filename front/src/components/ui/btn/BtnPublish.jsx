b9f6d3135154eee56f1a01325fb4d41f51d89e5a
bot <maximbotpy@gmail.com>
2025-01-31 23:50:19 +0300

    refactor: упростил компонент кнопки для публикации изображения

--- front/src/components/ui/btn/BtnPublish.jsx
+++ front/src/components/ui/btn/BtnPublish.jsx
@@ -1,12 +1,11 @@
 import { useRef } from "react"
 import { api } from "../../../api/config"
 import { useFetch } from "../../hook/useFetch"
-import { LoaderV2 } from "../../loader/LoaderV2"
 
-import "./style/btn_publish.sass"
+import { BtnSend } from "./BtnSend"
 
 
-export function BtnPublish({uuidImg, tagList}){
+export function BtnPublish({uuidImg, tagList}){    
     const isPublic = useRef(false)
 
     const [request, isLoading, error] = useFetch(
@@ -16,18 +15,8 @@ export function BtnPublish({uuidImg, tagList}){
         }
     )
     return(
-        <>
-        { isLoading
-            ? <LoaderV2/>
-            : isPublic.current
-                ?<p className="btn-publish btn-publish_pub">Изображение опубликовано</p>
-                :<button 
-                    className="btn-publish" onClick={async (event) => await request(event)}
-                    disabled={tagList ? false : true}
-                >
-                    Опубликовать
-                </button> 
-        }
-        </>
-    )
+        <BtnSend>
+            {isPublic.current ? "Изображение опубликовано" : "Опубликовать"}
+        </BtnSend>
+    ) 
 }
